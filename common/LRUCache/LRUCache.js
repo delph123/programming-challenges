c805bdc2776287021d791be7cae89baaaee371b2
Delphin Barraud <delphin.barraud@soprasteria.com>
2023-02-06 21:40:23 +0100

    Add editor config

--- common/LRUCache/LRUCache.js
+++ common/LRUCache/LRUCache.js
@@ -15,102 +15,102 @@
  * it could not be found in the cache).
  */
 class LRUCache {
-  /** The real cache */
-  cache;
-  /** The maximum capacity of the cache */
-  capacity;
-  /** A function to derive the value from the key, used during cache miss */
-  _onCacheMissed;
+    /** The real cache */
+    cache;
+    /** The maximum capacity of the cache */
+    capacity;
+    /** A function to derive the value from the key, used during cache miss */
+    _onCacheMissed;
 
-  constructor(capacity = 10000, { onCacheMissed } = {}) {
-    this.cache = new Map();
-    this.capacity = capacity;
-    this._onCacheMissed = onCacheMissed;
-  }
+    constructor(capacity = 10000, { onCacheMissed } = {}) {
+        this.cache = new Map();
+        this.capacity = capacity;
+        this._onCacheMissed = onCacheMissed;
+    }
 
-  /**
-   * Check if the element exists in the cache.
-   *
-   * Note: If a cache miss handler was provided, it won't be called by this
-   * method in case that there is no value in the cache.
-   *
-   * @returns whether an element with the specified key exists or not
-   */
-  has(key) {
-    return this.cache.has(key);
-  }
+    /**
+     * Check if the element exists in the cache.
+     *
+     * Note: If a cache miss handler was provided, it won't be called by this
+     * method in case that there is no value in the cache.
+     *
+     * @returns whether an element with the specified key exists or not
+     */
+    has(key) {
+        return this.cache.has(key);
+    }
 
-  /**
-   * Returns the specified element from the cache if it was found. If the key is not
-   * found, will try to deviate the value from the cache miss handler if possible.
-   *
-   * @returns the element associated with the specified key or undefined
-   */
-  get(key) {
-    // If we don't find the key in the cache, regenerate it
-    // from the _onCacheMissed handler if it was provided
-    if (!this.cache.has(key)) {
-      if (this._onCacheMissed) {
-        const v = this._onCacheMissed(key);
-        if (v !== undefined) {
-          this.set(key, v);
-          return v;
+    /**
+     * Returns the specified element from the cache if it was found. If the key is not
+     * found, will try to deviate the value from the cache miss handler if possible.
+     *
+     * @returns the element associated with the specified key or undefined
+     */
+    get(key) {
+        // If we don't find the key in the cache, regenerate it
+        // from the _onCacheMissed handler if it was provided
+        if (!this.cache.has(key)) {
+            if (this._onCacheMissed) {
+                const v = this._onCacheMissed(key);
+                if (v !== undefined) {
+                    this.set(key, v);
+                    return v;
+                }
+            }
+            return undefined;
         }
-      }
-      return undefined;
+
+        const val = this.cache.get(key);
+
+        // Remove & set the key again in the cache to update the
+        // order of this key and put it at the end (since it is
+        // the most recently used key now)
+        this.cache.delete(key);
+        this.cache.set(key, val);
+
+        return val;
     }
 
-    const val = this.cache.get(key);
+    /**
+     * Adds a new element with a specified key and value to the cache.
+     *
+     * If an element with the same key already exists, the element will
+     * be overwritten.
+     */
+    set(key, val) {
+        // Same thing as for get, we delete the key before to
+        // set it again, in order to maintain the key order
+        this.cache.delete(key);
 
-    // Remove & set the key again in the cache to update the
-    // order of this key and put it at the end (since it is
-    // the most recently used key now)
-    this.cache.delete(key);
-    this.cache.set(key, val);
+        if (this.cache.size >= this.capacity) {
+            // We've reached the capacity limitation, we'll need
+            // to remove the least recently used key. Since the
+            // underlying Map maintain the key in insertion order
+            // the least recently used key is the first key from
+            // the cache
+            this.cache.delete(this.cache.keys().next().value);
+        }
 
-    return val;
-  }
+        this.cache.set(key, val);
 
-  /**
-   * Adds a new element with a specified key and value to the cache.
-   *
-   * If an element with the same key already exists, the element will
-   * be overwritten.
-   */
-  set(key, val) {
-    // Same thing as for get, we delete the key before to
-    // set it again, in order to maintain the key order
-    this.cache.delete(key);
-
-    if (this.cache.size >= this.capacity) {
-      // We've reached the capacity limitation, we'll need
-      // to remove the least recently used key. Since the
-      // underlying Map maintain the key in insertion order
-      // the least recently used key is the first key from
-      // the cache
-      this.cache.delete(this.cache.keys().next().value);
+        return this;
     }
 
-    this.cache.set(key, val);
+    /**
+     * Delete / evict an element from the cache.
+     *
+     * @returns true if this element was in the cache and has been removed or false
+     */
+    delete(key) {
+        return this.cache.delete(key);
+    }
 
-    return this;
-  }
-
-  /**
-   * Delete / evict an element from the cache.
-   *
-   * @returns true if this element was in the cache and has been removed or false
-   */
-  delete(key) {
-    return this.cache.delete(key);
-  }
-
-  /**
-   * Clear the cache content.
-   */
-  clear() {
-    this.cache.clear();
-  }
+    /**
+     * Clear the cache content.
+     */
+    clear() {
+        this.cache.clear();
+    }
 }
 
 module.exports = LRUCache;
--- common/LRUCache/LRUCache.test.js
+++ common/LRUCache/LRUCache.test.js
@@ -4,115 +4,121 @@ const CACHE_CAPACITY = 5;
 let LRU_CACHE;
 
 beforeEach(() => {
-  // Recreate new LRU Cache
-  LRU_CACHE = new LRUCache(CACHE_CAPACITY);
-  LRU_CACHE.set("a", 1).set("b", 2).set("c", 3);
+    // Recreate new LRU Cache
+    LRU_CACHE = new LRUCache(CACHE_CAPACITY);
+    LRU_CACHE.set("a", 1).set("b", 2).set("c", 3);
 });
 
 afterAll(() => {
-  LRU_CACHE = undefined;
+    LRU_CACHE = undefined;
 });
 
 test("It accepts an empty constructor", () => {
-  expect(new LRUCache()).toBeInstanceOf(LRUCache);
+    expect(new LRUCache()).toBeInstanceOf(LRUCache);
 });
 
 test("It caches records provided", () => {
-  expect(LRU_CACHE.has("a")).toBe(true);
-  expect(LRU_CACHE.get("b")).toBe(2);
+    expect(LRU_CACHE.has("a")).toBe(true);
+    expect(LRU_CACHE.get("b")).toBe(2);
 });
 
 test("It returns undefined for unknown keys", () => {
-  expect(LRU_CACHE.has("x")).toBe(false);
-  expect(LRU_CACHE.get("y")).toBeUndefined();
+    expect(LRU_CACHE.has("x")).toBe(false);
+    expect(LRU_CACHE.get("y")).toBeUndefined();
 });
 
 test("It deletes keys", () => {
-  expect(LRU_CACHE.get("a")).toBe(1);
-  LRU_CACHE.delete("a");
-  expect(LRU_CACHE.get("a")).toBeUndefined();
-  expect(LRU_CACHE.get("b")).toBe(2);
-  LRU_CACHE.clear();
-  expect(LRU_CACHE.get("b")).toBeUndefined();
+    expect(LRU_CACHE.get("a")).toBe(1);
+    LRU_CACHE.delete("a");
+    expect(LRU_CACHE.get("a")).toBeUndefined();
+    expect(LRU_CACHE.get("b")).toBe(2);
+    LRU_CACHE.clear();
+    expect(LRU_CACHE.get("b")).toBeUndefined();
 });
 
 test("It removes oldest keys to keep capacity", () => {
-  for (let i = 0; i < CACHE_CAPACITY; i++) {
-    LRU_CACHE.set("k" + i, i);
-  }
+    for (let i = 0; i < CACHE_CAPACITY; i++) {
+        LRU_CACHE.set("k" + i, i);
+    }
 
-  expect(LRU_CACHE.has("a")).toBe(false);
-  expect(LRU_CACHE.get("c")).toBeUndefined();
+    expect(LRU_CACHE.has("a")).toBe(false);
+    expect(LRU_CACHE.get("c")).toBeUndefined();
 
-  for (let i = 0; i < CACHE_CAPACITY; i++) {
-    expect(LRU_CACHE.get("k" + i)).toBe(i);
-  }
+    for (let i = 0; i < CACHE_CAPACITY; i++) {
+        expect(LRU_CACHE.get("k" + i)).toBe(i);
+    }
 });
 
 test("It updates key order at read", () => {
-  // Reach capacity
-  LRU_CACHE.set("d", 4).set("e", 5);
-  // Read cache for "a" & "c"
-  expect(LRU_CACHE.get("a")).toBe(1);
-  expect(LRU_CACHE.get("c")).toBe(3);
-  // Overflow => LRU cache ("b" & "d") will be removed
-  LRU_CACHE.set("f", 6).set("g", 7);
-  // "b" and "d" are indeed gone
-  expect(LRU_CACHE.has("b")).toBe(false);
-  expect(LRU_CACHE.get("d")).toBeUndefined();
-  expect(LRU_CACHE.get("e")).toBe(5);
+    // Reach capacity
+    LRU_CACHE.set("d", 4).set("e", 5);
+    // Read cache for "a" & "c"
+    expect(LRU_CACHE.get("a")).toBe(1);
+    expect(LRU_CACHE.get("c")).toBe(3);
+    // Overflow => LRU cache ("b" & "d") will be removed
+    LRU_CACHE.set("f", 6).set("g", 7);
+    // "b" and "d" are indeed gone
+    expect(LRU_CACHE.has("b")).toBe(false);
+    expect(LRU_CACHE.get("d")).toBeUndefined();
+    expect(LRU_CACHE.get("e")).toBe(5);
 });
 
 test("It updates key order at write/replace", () => {
-  // Reach capacity
-  LRU_CACHE.set("d", 4).set("e", 5);
-  // Update cache for "a" & "c"
-  LRU_CACHE.set("a", 6).set("c", 7);
-  // Overflow => LRU cache ("b" & "d") will be removed
-  LRU_CACHE.set("f", 8).set("g", 9);
-  // "b" and "d" are indeed gone
-  expect(LRU_CACHE.has("b")).toBe(false);
-  expect(LRU_CACHE.get("d")).toBeUndefined();
-  expect(LRU_CACHE.get("e")).toBe(5);
+    // Reach capacity
+    LRU_CACHE.set("d", 4).set("e", 5);
+    // Update cache for "a" & "c"
+    LRU_CACHE.set("a", 6).set("c", 7);
+    // Overflow => LRU cache ("b" & "d") will be removed
+    LRU_CACHE.set("f", 8).set("g", 9);
+    // "b" and "d" are indeed gone
+    expect(LRU_CACHE.has("b")).toBe(false);
+    expect(LRU_CACHE.get("d")).toBeUndefined();
+    expect(LRU_CACHE.get("e")).toBe(5);
 });
 
 test("It accepts a cache miss handler", () => {
-  // Create a cache with a cache miss handler generating keys
-  // like: a -> 1, b -> 2, c -> 3, etc.
-  const cache = new LRUCache(CACHE_CAPACITY, {
-    onCacheMissed: (c) => c.charCodeAt(0) - "a".charCodeAt(0) + 1,
-  });
-  // Fill a few keys matching the pattern
-  cache.set("a", 1).set("b", 2).set("c", 3);
-  // Read the cache for twice it's size (keys will be generated on the fly)
-  for (let i = 0; i < 2 * CACHE_CAPACITY; i++) {
-    expect(cache.get(String.fromCharCode(i + "a".charCodeAt(0)))).toBe(i + 1);
-  }
-  // Oldest keys are gone
-  for (let i = 0; i < CACHE_CAPACITY; i++) {
-    expect(cache.has(String.fromCharCode(i + "a".charCodeAt(0)))).toBe(false);
-  }
-  // Newest keys are still there
-  for (let i = CACHE_CAPACITY; i < 2 * CACHE_CAPACITY; i++) {
-    expect(cache.has(String.fromCharCode(i + "a".charCodeAt(0)))).toBe(true);
-  }
+    // Create a cache with a cache miss handler generating keys
+    // like: a -> 1, b -> 2, c -> 3, etc.
+    const cache = new LRUCache(CACHE_CAPACITY, {
+        onCacheMissed: (c) => c.charCodeAt(0) - "a".charCodeAt(0) + 1,
+    });
+    // Fill a few keys matching the pattern
+    cache.set("a", 1).set("b", 2).set("c", 3);
+    // Read the cache for twice it's size (keys will be generated on the fly)
+    for (let i = 0; i < 2 * CACHE_CAPACITY; i++) {
+        expect(cache.get(String.fromCharCode(i + "a".charCodeAt(0)))).toBe(
+            i + 1
+        );
+    }
+    // Oldest keys are gone
+    for (let i = 0; i < CACHE_CAPACITY; i++) {
+        expect(cache.has(String.fromCharCode(i + "a".charCodeAt(0)))).toBe(
+            false
+        );
+    }
+    // Newest keys are still there
+    for (let i = CACHE_CAPACITY; i < 2 * CACHE_CAPACITY; i++) {
+        expect(cache.has(String.fromCharCode(i + "a".charCodeAt(0)))).toBe(
+            true
+        );
+    }
 });
 
 test("It doesn't store the handler's result when undefined", () => {
-  // Create a cache with a cache miss handler generating keys
-  // for pair numbers only
-  const cache = new LRUCache(CACHE_CAPACITY, {
-    onCacheMissed: (c) => (c % 2 === 0 ? c : undefined),
-  });
-  // Fill a few keys matching the pattern
-  cache.set(1, 1).set(2, 2);
-  // Check keys from cache
-  expect(cache.get(1)).toBe(1);
-  expect(cache.get(2)).toBe(2);
-  // Check pair key for cache miss handler
-  expect(cache.get(4)).toBe(4);
-  expect(cache.has(4)).toBe(true);
-  // Check impair key for cache miss handler
-  expect(cache.get(5)).toBeUndefined();
-  expect(cache.has(5)).toBe(false);
+    // Create a cache with a cache miss handler generating keys
+    // for pair numbers only
+    const cache = new LRUCache(CACHE_CAPACITY, {
+        onCacheMissed: (c) => (c % 2 === 0 ? c : undefined),
+    });
+    // Fill a few keys matching the pattern
+    cache.set(1, 1).set(2, 2);
+    // Check keys from cache
+    expect(cache.get(1)).toBe(1);
+    expect(cache.get(2)).toBe(2);
+    // Check pair key for cache miss handler
+    expect(cache.get(4)).toBe(4);
+    expect(cache.has(4)).toBe(true);
+    // Check impair key for cache miss handler
+    expect(cache.get(5)).toBeUndefined();
+    expect(cache.has(5)).toBe(false);
 });
