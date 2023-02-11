6b2d4a38d336a46022043fb9338972ae73617cbe
Delphin Barraud <delphin.barraud@soprasteria.com>
2023-02-11 22:36:31 +0100

    Add a method to reverse the order of the Heap

--- common/Heap/Heap.js
+++ common/Heap/Heap.js
@@ -9,7 +9,7 @@ Heap.prototype.merge = function merge(values) {
     this.content.push(...values);
     this.length += values.length;
 
-    if (k * Math.log2(n+k) > n + k) {
+    if (k * Math.log2(n + k) > n + k) {
         let m = Math.trunc(this.length / 2) - 1;
         for (let i = m; i >= 0; i--) {
             this.sink(i);
@@ -19,7 +19,7 @@ Heap.prototype.merge = function merge(values) {
             this.float(i);
         }
     }
-}
+};
 
 Heap.prototype.addEach = function addEach(values, mapFn, thisp) {
     let newValues = [];
@@ -29,10 +29,25 @@ Heap.prototype.addEach = function addEach(values, mapFn, thisp) {
     _addEach.call(this, values, mapFn, thisp);
     delete this.add;
     this.merge(newValues);
-}
+};
 
 Heap.prototype.push = function push(...values) {
     this.merge(values);
-}
+};
+
+Heap.prototype.reverse = function reverse() {
+    // Inverse the order
+    let oldContentCompare = this.contentCompare;
+    this.contentCompare = function contentCompare(a, b) {
+        return -oldContentCompare.call(this, a, b);
+    };
+    // Re-heapify the elements according to the new order
+    let m = Math.trunc(this.length / 2) - 1;
+    for (let i = m; i >= 0; i--) {
+        this.sink(i);
+    }
+    // return current reference as result
+    return this;
+};
 
 module.exports = Heap;
