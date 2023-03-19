41a2b56ef73c8c5869a2ec967257d30dd71b403d
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2023-03-19 23:17:32 +0100

    Add ex1 of 2018 final (arbitrage trading)

--- MDF2023/02_training/MDF2018/Final/ex1_arbitrage_trading.js
+++ MDF2023/02_training/MDF2018/Final/ex1_arbitrage_trading.js
@@ -14,5 +14,30 @@ readline_object.on("line", (value) => {
 readline_object.on("close", ContestResponse);
 
 function ContestResponse() {
-    //implement your code here using input array
+    const [, trades] = input[0].split(" ").map((n) => parseInt(n));
+    const rates = input
+        .slice(1)
+        .map((l) => l.split(" ").map((a) => parseFloat(a)));
+
+    const start = new Array(rates.length)
+        .fill(0)
+        .map((_, i) => (i === 0 ? 10000 : 0));
+
+    console.error(trades, rates);
+
+    console.log(maxAmount(rates, trades, start)[0]);
+}
+
+function maxAmount(rates, trades, start) {
+    let amounts = start;
+
+    for (let t = 0; t < trades; t++) {
+        amounts = amounts.map((_, i) => {
+            return amounts.reduce((bestTrade, a, j) => {
+                return Math.max(bestTrade, a * rates[j][i]);
+            }, 0);
+        });
+    }
+
+    return amounts;
 }
