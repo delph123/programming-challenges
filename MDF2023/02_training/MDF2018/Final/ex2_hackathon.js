a65eefa9c1c8de9589e172c25ba06951f21e8f29
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2023-03-20 00:20:52 +0100

    cleanup

--- MDF2023/02_training/MDF2018/Final/ex2_hackathon.js
+++ MDF2023/02_training/MDF2018/Final/ex2_hackathon.js
@@ -14,56 +14,38 @@ readline_object.on("line", (value) => {
 readline_object.on("close", ContestResponse);
 
 function ContestResponse() {
-    const N = parseInt(input[0]);
     const meet = input
         .slice(1)
         .map((others) => others.split(" ").map((n) => n === "1"));
 
-    //   console.error(N, meet)
+    const participants = new Set(
+        new Array(meet.length).fill(0).map((_, i) => i)
+    );
 
-    //   const all = new Array(N)
-    //     .fill(0)
-    //     .map((_, i) => new Set(meet[i].map((_, j) => j).filter((j) => meet[i][j])));
+    while (true) {
+        let candidate = -1;
+        let min_neighbours = participants.size + 1;
 
-    const all = [new Set(new Array(N).fill(0).map((_, i) => i))];
-
-    //   console.error(meet);
-    //   console.error(all);
-
-    all.forEach((s) => {
-        let stop = false;
-        while (!stop) {
-            let m = new Map();
-
-            for (const val of s) {
-                let nb = 0;
-                for (const other of s) {
-                    if (meet[val][other]) {
-                        nb++;
-                    }
+        for (const neighbour of participants) {
+            let nb = 0;
+            for (const other of participants) {
+                if (meet[neighbour][other]) {
+                    nb++;
                 }
-                m.set(val, nb);
             }
-
-            const [wnei, wnb] = [...m.entries()].reduce(
-                ([nei, nn], [mnei, mnn]) => {
-                    if (nn < mnn) {
-                        return [nei, nn];
-                    } else {
-                        return [mnei, mnn];
-                    }
-                },
-                [-1, s.size + 1]
-            );
-
-            if (wnb < s.size) {
-                s.delete(wnei);
-            } else {
-                stop = true;
+            if (nb < min_neighbours) {
+                candidate = neighbour;
+                min_neighbours = nb;
             }
         }
-    });
 
-    console.error(all, Math.max(...all.map((s) => s.size)));
-    console.log(Math.max(...all.map((s) => s.size)));
+        if (min_neighbours < participants.size) {
+            participants.delete(candidate);
+        } else {
+            break;
+        }
+    }
+
+    // console.error(all, all.size);
+    console.log(participants.size);
 }
