8ab55fa69852b70c5a62f893ced6415a4d430465
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2023-03-19 20:55:28 +0100

    Solution for MDF 2023 / ex 3 / final

--- MDF2023/04_final/ex3_number_of_alternatives.js
+++ MDF2023/04_final/ex3_number_of_alternatives.js
@@ -1,52 +1,357 @@
-const input = [
-    "10",
-    "1 marketing computers",
-    "2 dancing dancing",
-    "3 computers customer_relationship",
-    "4 music entertainment",
-    "5 bread sausages",
-    "6 computers journalism",
-    "7 advertising web_scrapping",
-    "8 dancing video",
-    "9 books children_books",
-    "10 cars electric_cars",
-];
+let input;
 
-let business;
+input = [
+    "input 1 > expected result 2",
+    "Ecommerce VirtualReality",
+    "Fintech B2BSoftware",
+    "Fintech Ecommerce",
+    "Ecommerce Ecommerce",
+]; // expected 2
+ContestResponse();
 
+input = [
+    "input 2 > expected result 512",
+    "Ecommerce B2BSoftware",
+    "Fintech Fintech",
+    "VirtualReality Logistics",
+    "Edtech AI",
+    "AI Edtech",
+    "RealEstate DeliveryServices",
+    "AI Fintech",
+    "VirtualReality Logistics",
+    "HeathcareTech AI",
+    "Entertainment BigData",
+]; // expect 512
+ContestResponse();
+
+input = ["input 3 > expected result 2", "Ecommerce Ecommerce"]; // expect 2
+ContestResponse();
+
+input = ["input 4 > expected result 2", "Ecommerce Edtech"]; // expect 2
+ContestResponse();
+
+input = [
+    "input 5 > expected result 0",
+    "VirtualReality BigData",
+    "VirtualReality VirtualReality",
+    "BigData BigData",
+    "BigData VirtualReality",
+    "BigData VirtualReality",
+    "BigData VirtualReality",
+    "BigData BigData",
+    "BigData BigData",
+    "BigData VirtualReality",
+    "BigData BigData",
+    "BigData VirtualReality",
+    "VirtualReality VirtualReality",
+    "BigData VirtualReality",
+    "BigData VirtualReality",
+    "BigData BigData",
+    "VirtualReality BigData",
+    "BigData VirtualReality",
+    "VirtualReality VirtualReality",
+    "VirtualReality VirtualReality",
+    "BigData VirtualReality",
+    "BigData VirtualReality",
+    "VirtualReality BigData",
+    "VirtualReality BigData",
+    "VirtualReality VirtualReality",
+    "BigData VirtualReality",
+    "BigData VirtualReality",
+    "BigData VirtualReality",
+    "VirtualReality VirtualReality",
+    "BigData VirtualReality",
+    "VirtualReality BigData",
+]; // expected 0
+ContestResponse();
+
+input = ["input 6 > expected result 0"]; // expected 0
+ContestResponse();
+
+input = [
+    "input 7 > expected result 19906560",
+    "B2BSoftware Entertainment",
+    "BigData Edtech",
+    "BigData RealEstate",
+    "B2BSoftware Entertainment",
+    "Edtech DeliveryService",
+    "VirtualReality RealEstate",
+    "DeliveryService VirtualReality",
+    "DeliveryService B2BSoftware",
+    "Ecommerce B2BSoftware",
+    "BigData BigData",
+    "BigData VirtualReality",
+    "RealEstate Ecommerce",
+    "SharedMobility Edtech",
+    "VirtualReality BigData",
+    "VirtualReality RealEstate",
+    "DeliveryService Edtech",
+    "SharedMobility SharedMobility",
+    "VirtualReality VirtualReality",
+    "BigData Entertainment",
+    "Entertainment AI",
+    "RealEstate VirtualReality",
+    "SharedMobility VirtualReality",
+    "AI SharedMobility",
+    "Entertainment DeliveryService",
+    "AI Edtech",
+    "Entertainment AI",
+    "Edtech Edtech",
+    "AI Entertainment",
+    "Ecommerce DeliveryService",
+    "Edtech DeliveryService",
+]; // expected 19906560
+ContestResponse();
+
+input = [
+    "input 8 > expected result 10651768002183168",
+    "Logistics VirtualReality",
+    "B2BSoftware DeliveryService",
+    "BigData RealEstate",
+    "HealthcareTech RealEstate",
+    "crlivsdqeuboqfyxertetyxvoaibmh Logistics",
+    "Ecommerce Logistics",
+    "B2BSoftware VirtualReality",
+    "Logistics Fintech",
+    "uhmvevizlclgdsxanflwsnfooxsikx VirtualReality",
+    "RealEstate SharedMobility",
+    "BigData Edtech",
+    "Ecommerce RealEstate",
+    "RealEstate Edtech",
+    "VirtualReality VirtualReality",
+    "BigData crlivsdqeuboqfyxertetyxvoaibmh",
+    "Fintech BigData",
+    "AI Logistics",
+    "Entertainment SharedMobility",
+    "crlivsdqeuboqfyxertetyxvoaibmh Logistics",
+    "SharedMobility Fintech",
+    "B2BSoftware Fintech",
+    "Entertainment VirtualReality",
+    "crlivsdqeuboqfyxertetyxvoaibmh AI",
+    "B2BSoftware AI",
+    "HealthcareTech Fintech",
+    "BigData crlivsdqeuboqfyxertetyxvoaibmh",
+    "BigData Entertainment",
+    "VirtualReality Entertainment",
+    "Entertainment DeliveryService",
+    "VirtualReality RealEstate",
+    "AI BigData",
+    "HealthcareTech Edtech",
+    "Entertainment Fintech",
+    "Entertainment RealEstate",
+    "Edtech crlivsdqeuboqfyxertetyxvoaibmh",
+    "AI Logistics",
+    "DeliveryService DeliveryService",
+    "Fintech Edtech",
+    "BigData Fintech",
+    "Entertainment uhmvevizlclgdsxanflwsnfooxsikx",
+    "Entertainment Fintech",
+    "Logistics B2BSoftware",
+    "VirtualReality SharedMobility",
+    "HealthcareTech Fintech",
+    "AI Entertainment",
+    "VirtualReality crlivsdqeuboqfyxertetyxvoaibmh",
+    "VirtualReality BigData",
+    "crlivsdqeuboqfyxertetyxvoaibmh Entertainment",
+    "Fintech crlivsdqeuboqfyxertetyxvoaibmh",
+    "RealEstate BigData",
+    "AI Logistics",
+    "SharedMobility Ecommerce",
+    "BigData uhmvevizlclgdsxanflwsnfooxsikx",
+    "crlivsdqeuboqfyxertetyxvoaibmh Fintech",
+    "AI HealthcareTech",
+    "B2BSoftware BigData",
+    "HealthcareTech Ecommerce",
+    "VirtualReality SharedMobility",
+    "uhmvevizlclgdsxanflwsnfooxsikx VirtualReality",
+    "Edtech uhmvevizlclgdsxanflwsnfooxsikx",
+]; // expected 10651768002183168
+ContestResponse();
+
+input = [
+    "input 9 > expected result 864691128455135232",
+    "BigData VirtualReality",
+    "Ecommerce Entertainment",
+    "B2BSoftware SharedMobility",
+    "DeliveryService RealEstate",
+    "Edtech AI",
+    "Logistics HealthcareTech",
+    "Fintech BigData",
+    "VirtualReality Ecommerce",
+    "Entertainment B2BSoftware",
+    "SharedMobility DeliveryService",
+    "RealEstate Edtech",
+    "AI Logistics",
+    "HealthcareTech Fintech",
+    "BigData VirtualReality",
+    "Ecommerce Entertainment",
+    "B2BSoftware SharedMobility",
+    "DeliveryService RealEstate",
+    "Edtech AI",
+    "Logistics HealthcareTech",
+    "Fintech BigData",
+    "VirtualReality Ecommerce",
+    "Entertainment B2BSoftware",
+    "SharedMobility DeliveryService",
+    "RealEstate Edtech",
+    "AI Logistics",
+    "HealthcareTech Fintech",
+    "BigData VirtualReality",
+    "Ecommerce Entertainment",
+    "B2BSoftware SharedMobility",
+    "DeliveryService RealEstate",
+    "Edtech AI",
+    "Logistics HealthcareTech",
+    "Fintech BigData",
+    "VirtualReality Ecommerce",
+    "Entertainment B2BSoftware",
+    "SharedMobility DeliveryService",
+    "RealEstate Edtech",
+    "AI Logistics",
+    "HealthcareTech Fintech",
+    "BigData VirtualReality",
+    "Ecommerce Entertainment",
+    "B2BSoftware SharedMobility",
+    "DeliveryService RealEstate",
+    "Edtech AI",
+    "Logistics HealthcareTech",
+    "Fintech BigData",
+    "VirtualReality Ecommerce",
+    "Entertainment B2BSoftware",
+    "SharedMobility DeliveryService",
+    "RealEstate Edtech",
+    "AI Logistics",
+    "HealthcareTech Fintech",
+    "BigData VirtualReality",
+    "Ecommerce Entertainment",
+    "B2BSoftware SharedMobility",
+    "DeliveryService RealEstate",
+    "Edtech AI",
+    "Logistics HealthcareTech",
+    "Fintech BigData",
+    "VirtualReality Ecommerce",
+]; // expected 864691128455135232
+ContestResponse();
+
+input = [
+    "input 10 > expected result 0",
+    "BigData VirtualReality",
+    "Ecommerce Entertainment",
+    "B2BSoftware B2BSoftware",
+    "B2BSoftware SharedMobility",
+    "SharedMobility SharedMobility",
+    "DeliveryService RealEstate",
+    "Edtech AI",
+    "Logistics HealthcareTech",
+    "Fintech BigData",
+    "VirtualReality Ecommerce",
+    "Entertainment B2BSoftware",
+    "B2BSoftware B2BSoftware",
+    "SharedMobility SharedMobility",
+    "SharedMobility DeliveryService",
+    "RealEstate Edtech",
+    "AI Logistics",
+    "HealthcareTech Fintech",
+    "BigData VirtualReality",
+    "Ecommerce Entertainment",
+    "B2BSoftware B2BSoftware",
+    "B2BSoftware SharedMobility",
+    "SharedMobility SharedMobility",
+    "DeliveryService RealEstate",
+    "Edtech AI",
+    "Logistics HealthcareTech",
+    "Fintech BigData",
+    "VirtualReality Ecommerce",
+    "Entertainment B2BSoftware",
+    "B2BSoftware B2BSoftware",
+    "SharedMobility SharedMobility",
+    "SharedMobility DeliveryService",
+    "RealEstate Edtech",
+    "AI Logistics",
+    "HealthcareTech Fintech",
+    "BigData VirtualReality",
+    "Ecommerce Entertainment",
+    "B2BSoftware B2BSoftware",
+    "B2BSoftware SharedMobility",
+    "SharedMobility SharedMobility",
+    "DeliveryService RealEstate",
+    "Edtech AI",
+    "Logistics HealthcareTech",
+    "Fintech BigData",
+    "VirtualReality Ecommerce",
+    "Entertainment B2BSoftware",
+    "B2BSoftware B2BSoftware",
+    "SharedMobility SharedMobility",
+    "SharedMobility DeliveryService",
+    "RealEstate Edtech",
+    "AI Logistics",
+    "HealthcareTech Fintech",
+    "BigData VirtualReality",
+    "Ecommerce Entertainment",
+    "B2BSoftware B2BSoftware",
+    "B2BSoftware SharedMobility",
+    "SharedMobility SharedMobility",
+    "DeliveryService RealEstate",
+    "Edtech AI",
+    "Logistics HealthcareTech",
+    "Fintech BigData",
+]; // expected 0
 ContestResponse();
 
 function ContestResponse() {
-    business = input.slice(1).map((line) => line.split(" "));
+    const alts = input.slice(1).map((line) => line.split(" "));
 
-    console.log(count(business.length - 1, new Set()));
+    console.error(input[0]);
+
+    if (alts.length === 0) {
+        console.log("0");
+    } else if (alts.length === 1) {
+        console.log("2");
+    } else {
+        const [a, b] = alts[alts.length - 1];
+        const altCntMap = new Map();
+
+        console.log(
+            (
+                count(altCntMap, alts, alts.length - 2, a, a) +
+                count(altCntMap, alts, alts.length - 2, b, b)
+            ).toString()
+        );
+    }
 }
 
-function count(idx, exn) {
-    const [, a, b] = business[idx];
+function count(altCntMap, alts, idx, first, prev) {
+    const [a, b] = alts[idx];
 
-    if (exn.has(a) && exn.has(b)) {
-        return 0;
-    } else if (exn.has(a)) {
-        if (idx === 0) {
-            return 1;
+    const key = idx + "/" + first + "/" + prev;
+    if (altCntMap.has(key)) {
+        return altCntMap.get(key);
+    }
+
+    if (idx === 0) {
+        if ((prev === a || first === a) && (prev === b || first === b)) {
+            return 0n;
+        } else if (prev === a || first === a) {
+            return 1n;
+        } else if (prev === b || first === b) {
+            return 1n;
         } else {
-            return count(idx - 1, new Set([...exn, a]));
-        }
-    } else if (exn.has(b)) {
-        if (idx === 0) {
-            return 1;
-        } else {
-            return count(idx - 1, new Set([...exn, b]));
+            return 2n;
         }
     } else {
-        if (idx === 0) {
-            return 2;
+        let cnt;
+        if (prev === a && prev === b) {
+            cnt = 0n;
+        } else if (prev === b) {
+            cnt = count(altCntMap, alts, idx - 1, first, a);
+        } else if (prev === a) {
+            cnt = count(altCntMap, alts, idx - 1, first, b);
         } else {
-            return (
-                count(idx - 1, new Set([...exn, a])) +
-                count(idx - 1, new Set([...exn, b]))
-            );
+            cnt =
+                count(altCntMap, alts, idx - 1, first, a) +
+                count(altCntMap, alts, idx - 1, first, b);
         }
+        altCntMap.set(key, cnt);
+        return cnt;
     }
 }
