c34b624894d8cdf5e1fffe6c5855d9e1e7bd0400
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-01-03 00:18:49 +0100

    Day 6 - Light grid

--- AdventOfCode/2015/day06.py
+++ AdventOfCode/2015/day06.py
@@ -1,11 +1,52 @@
 # Parse input
 
-input = open("AdventOfCode/2015/examples/day06.in").read().strip()
+instructions = [
+    (
+        l.replace("turn ", "turn_").split(" ")[0],
+        tuple(int(n) for n in l.replace("turn ", "turn_").split(" ")[1].split(",")),
+        tuple(int(n) for n in l.split(" through ")[1].split(",")),
+    )
+    for l in open("AdventOfCode/2015/examples/day06.in").read().strip().split("\n")
+]
 
 # Part 1
 
-print("Part 1:", input)
+
+def lit():
+    lights = [[0 for _ in range(1000)] for _ in range(1000)]
+    for instruction, (a, b), (c, d) in instructions:
+        assert a <= c and b <= d
+        for i in range(a, c + 1):
+            for j in range(b, d + 1):
+                lights[i][j] = (
+                    1
+                    if instruction == "turn_on"
+                    else (0 if instruction == "turn_off" else 1 - lights[i][j])
+                )
+    return lights
+
+
+print("Part 1:", sum([sum(l) for l in lit()]))
 
 # Part 2
 
-print("Part 2:", 0)
+
+def lit_p2():
+    lights = [[0 for _ in range(1000)] for _ in range(1000)]
+    for instruction, (a, b), (c, d) in instructions:
+        assert a <= c and b <= d
+        for i in range(a, c + 1):
+            for j in range(b, d + 1):
+                lights[i][j] += (
+                    1
+                    if instruction == "turn_on"
+                    else (
+                        2
+                        if instruction == "toggle"
+                        else (-1 if lights[i][j] >= 1 else 0)
+                    )
+                )
+    return lights
+
+
+print("Part 2:", sum([sum(l) for l in lit_p2()]))
