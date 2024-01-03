aafeddcf2dd58198cdd27130024e3c5e25f5a63e
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-01-03 18:04:24 +0100

    Day 9 - Travelling salesman

--- AdventOfCode/2015/day09.py
+++ AdventOfCode/2015/day09.py
@@ -1,11 +1,50 @@
 # Parse input
 
-input = open("AdventOfCode/2015/examples/day09.in").read().strip()
+distances = [
+    (l.split(" = ")[0].split(" to "), int(l.split(" = ")[1]))
+    for l in open("AdventOfCode/2015/examples/day09.in").read().strip().split("\n")
+]
+
+distances = {(a, b): d for [a, b], d in distances} | {
+    (b, a): d for [a, b], d in distances
+}
+
+
+def graph():
+    vertices = set(sum([list(e) for e in distances.keys()], []))
+    return {
+        n1: {n2: distances[(n1, n2)] for n2 in vertices if n2 != n1} for n1 in vertices
+    }
+
+
+G = graph()
+MAX = len(G) * max(distances.values()) + 1
+
 
 # Part 1
 
-print("Part 1:", input)
+
+def shortest_dist(start, excl):
+    dist = MAX
+    for n, d in G[start].items():
+        if n in excl:
+            continue
+        dist = min(dist, d + shortest_dist(n, excl | {n}))
+    return dist if dist < MAX else 0
+
+
+print("Part 1:", min(shortest_dist(city, {city}) for city in G.keys()))
 
 # Part 2
 
-print("Part 2:", 0)
+
+def longest_dist(start, excl):
+    dist = 0
+    for n, d in G[start].items():
+        if n in excl:
+            continue
+        dist = max(dist, d + longest_dist(n, excl | {n}))
+    return dist
+
+
+print("Part 2:", max(longest_dist(city, {city}) for city in G.keys()))
