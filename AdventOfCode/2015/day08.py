c205405a94704e0628ec3c60af7441fd29e2b681
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-01-03 17:27:36 +0100

    Day 8 - String representation

--- AdventOfCode/2015/day08.py
+++ AdventOfCode/2015/day08.py
@@ -1,11 +1,11 @@
 # Parse input
 
-input = open("AdventOfCode/2015/examples/day08.in").read().strip()
+list = open("AdventOfCode/2015/inputs/day08.in").read().strip().split("\n")
 
 # Part 1
 
-print("Part 1:", input)
+print("Part 1:", sum([len(l) - len(eval(l)) for l in list]))
 
 # Part 2
 
-print("Part 2:", 0)
+print("Part 2:", sum([2 + l.count('"') + l.count("\\") for l in list]))
