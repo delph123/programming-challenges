feb132e22b35101cb452cc9809db928b86bafb2c
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-01-03 11:16:06 +0100

    Part 7 - Bitwise logic gates

--- AdventOfCode/2015/day07.py
+++ AdventOfCode/2015/day07.py
@@ -1,11 +1,50 @@
+from functools import cache
+
 # Parse input
 
-input = open("AdventOfCode/2015/examples/day07.in").read().strip()
+instructions = {
+    conn.split(" -> ")[1]: conn.split(" -> ")[0]
+    for conn in open("AdventOfCode/2015/examples/day07.in").read().strip().split("\n")
+}
 
 # Part 1
 
-print("Part 1:", input)
+
+def parse(conn: str, get_value):
+    if " " not in conn:
+        return get_value(conn, get_value)
+    if conn.startswith("NOT "):
+        return 65536 + ~int(get_value(conn[4:], get_value))
+    a, op, b = conn.split(" ")
+    va = get_value(a, get_value)
+    vb = get_value(b, get_value)
+    match op:
+        case "AND":
+            return va & vb
+        case "OR":
+            return va | vb
+        case "LSHIFT":
+            return va << vb
+        case "RSHIFT":
+            return va >> vb
+
+
+@cache
+def value(label: str, get_value):
+    if label.isdecimal():
+        return int(label)
+    return parse(instructions[label], get_value)
+
+
+print("Part 1:", value("a", value))
 
 # Part 2
 
-print("Part 2:", 0)
+
+def value_p2(label: str, _):
+    if label == "b":
+        return value("a", value)
+    return value(label, value_p2)
+
+
+print("Part 2:", value_p2("a", value_p2))
