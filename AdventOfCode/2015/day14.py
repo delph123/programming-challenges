ba18768845c9f9670993cc9ae844a78e5b34a2d4
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-01-03 22:47:40 +0100

    Day 14 - Race

--- AdventOfCode/2015/day14.py
+++ AdventOfCode/2015/day14.py
@@ -1,11 +1,50 @@
 # Parse input
 
-input = open("AdventOfCode/2015/examples/day14.in").read().strip()
+speeds = [
+    l.replace("can fly ", "")
+    .replace("km/s for ", "")
+    .replace("seconds, but then must rest for ", "")
+    .replace(" seconds.", "")
+    .split(" ")
+    for l in open("AdventOfCode/2015/inputs/day14.in").read().strip().split("\n")
+]
+
+speeds = {
+    reindeer: (int(speed), int(duration), int(rest))
+    for (reindeer, speed, duration, rest) in speeds
+}
 
 # Part 1
 
-print("Part 1:", input)
+
+def distance(reindeer, duration):
+    speed, dur, rest = speeds[reindeer]
+    bursts = duration // (dur + rest)
+    remainder = duration % (dur + rest)
+    if remainder >= dur:
+        return (bursts + 1) * speed * dur
+    else:
+        return bursts * speed * dur + remainder * speed
+
+
+print("Part 1:", max([distance(r, 2503) for r in speeds.keys()]))
 
 # Part 2
 
-print("Part 2:", 0)
+
+def race(duration):
+    reindeers = list(speeds.keys())
+    points = {r: 0 for r in reindeers}
+    dist = {r: 0 for r in reindeers}
+    for d in range(duration):
+        for reindeer in reindeers:
+            speed, dur, rest = speeds[reindeer]
+            remainder = d % (dur + rest)
+            dist[reindeer] += speed if remainder < dur else 0
+        winner = max(dist.values())
+        for reindeer in reindeers:
+            points[reindeer] += 1 if dist[reindeer] == winner else 0
+    return points
+
+
+print("Part 2:", max(race(2503).values()))
