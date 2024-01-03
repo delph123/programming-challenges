cffc3b0e53e5e90d71473488a63d3dd7b21b5467
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-01-03 19:03:28 +0100

    Day 10 - Conway sequence

--- AdventOfCode/2015/day10.py
+++ AdventOfCode/2015/day10.py
@@ -1,11 +1,35 @@
 # Parse input
 
-input = open("AdventOfCode/2015/examples/day10.in").read().strip()
+sequence = open("AdventOfCode/2015/examples/day10.in").read().strip()
 
 # Part 1
 
-print("Part 1:", input)
+
+def say(seq):
+    prev = ""
+    times = 0
+    res = []
+    for s in seq:
+        if s != prev:
+            if times > 0:
+                res.append(str(times) + prev)
+            prev = s
+            times = 1
+        else:
+            times += 1
+    if times > 0:
+        res.append(str(times) + prev)
+    return "".join(res)
+
+
+def repeat(seq, times):
+    for _ in range(times):
+        seq = say(seq)
+    return seq
+
+
+print("Part 1:", len(repeat(sequence, 40)))
 
 # Part 2
 
-print("Part 2:", 0)
+print("Part 2:", len(repeat(sequence, 50)))
