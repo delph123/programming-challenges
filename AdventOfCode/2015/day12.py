04b90437b82a6a1bef9be3c57a7525b07789edd3
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-01-03 21:23:00 +0100

    Day 12 - Introspection

--- AdventOfCode/2015/day12.py
+++ AdventOfCode/2015/day12.py
@@ -1,11 +1,32 @@
+import json
+
 # Parse input
 
-input = open("AdventOfCode/2015/examples/day12.in").read().strip()
+books = json.load(open("AdventOfCode/2015/examples/day12.in"))
 
 # Part 1
 
-print("Part 1:", input)
+
+def parse(books, excl=None):
+    if isinstance(books, dict):
+        if excl is not None and excl in books.values():
+            return 0
+        s = 0
+        for k, v in books.items():
+            s += parse(k, excl) + parse(v, excl)
+        return s
+    if isinstance(books, list):
+        s = 0
+        for v in books:
+            s += parse(v, excl)
+        return s
+    if isinstance(books, int):
+        return books
+    return 0
+
+
+print("Part 1:", parse(books))
 
 # Part 2
 
-print("Part 2:", 0)
+print("Part 2:", parse(books, "red"))
