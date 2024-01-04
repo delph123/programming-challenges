ff75e497d680a21446f4ba544c93144938685da0
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-01-04 11:14:56 +0100

    Day 16 - Fuzzy match

--- AdventOfCode/2015/day16.py
+++ AdventOfCode/2015/day16.py
@@ -1,11 +1,57 @@
 # Parse input
 
-input = open("AdventOfCode/2015/examples/day16.in").read().strip()
+detected = """
+    children: 3
+    cats: 7
+    samoyeds: 2
+    pomeranians: 3
+    akitas: 0
+    vizslas: 0
+    goldfish: 5
+    trees: 3
+    cars: 2
+    perfumes: 1
+"""
+
+detected = {
+    l.strip().split(": ")[0]: int(l.strip().split(": ")[1])
+    for l in detected.strip().split("\n")
+}
+
+sues = [
+    l.strip().replace(": ", "|", 1).split("|")
+    for l in open("AdventOfCode/2015/examples/day16.in").read().strip().split("\n")
+]
+
+sues = {
+    s[4:]: {
+        l.strip().split(": ")[0]: int(l.strip().split(": ")[1])
+        for l in p.strip().split(", ")
+    }
+    for s, p in sues
+}
 
 # Part 1
 
-print("Part 1:", input)
+
+def match(sue):
+    return all(detected[c] == v for c, v in sue.items())
+
+
+print("Part 1:", list({sue for sue, caracs in sues.items() if match(caracs)})[0])
 
 # Part 2
 
-print("Part 2:", 0)
+
+def match_p2(sue):
+    def matcher(c, v, d):
+        if c in ["cats", "trees"]:
+            return v > d
+        if c in ["pomeranians", "goldfish"]:
+            return v < d
+        return v == d
+
+    return all(matcher(c, v, detected[c]) for c, v in sue.items())
+
+
+print("Part 2:", list({sue for sue, caracs in sues.items() if match_p2(caracs)})[0])
