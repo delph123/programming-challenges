ef1c13daa2ddf8b62fee815bdddc522eebf83571
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-01-06 11:02:24 +0100

    Day 20 - Divisors

--- AdventOfCode/2015/day20.py
+++ AdventOfCode/2015/day20.py
@@ -1,13 +1,24 @@
 from libs import *
+from itertools import dropwhile, count
 
 # Parse input
 
-input = read("input")
+minimum_presents = int(read("example"))
 
 # Part 1
 
-part_one(input)
+
+def house_presents(n):
+    return 10 * sum(divisors(n))
+
+
+part_one(next(dropwhile(lambda n: house_presents(n) < minimum_presents, count())))
 
 # Part 2
 
-part_two(0)
+
+def house_presents_p2(n):
+    return 11 * sum([m for m in divisors(n) if n <= 50 * m])
+
+
+part_two(next(dropwhile(lambda n: house_presents_p2(n) < minimum_presents, count())))
