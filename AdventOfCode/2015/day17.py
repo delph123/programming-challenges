4168fc948b818e7bd51d1726b449db980c950fc2
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-01-07 21:46:32 +0100

    Day 17 - Combinations

--- AdventOfCode/2015/day17.py
+++ AdventOfCode/2015/day17.py
@@ -1,32 +1,22 @@
+from libs import *
+
 # Parse input
 
-containers = [
-    int(n)
-    for n in open("AdventOfCode/2015/examples/day17.in").read().strip().split("\n")
-]
+containers = [int(n) for n in read("example").split("\n")]
 
 # Part 1
 
 
 def fill(volume, containers):
-    if volume == 0:
-        return [[]]
-    if len(containers) == 1:
-        return [list(containers)] if volume == containers[0] else []
-    combinations = []
-    if volume >= containers[0]:
-        combs = fill(volume - containers[0], containers[1:])
-        combinations.extend([containers[0]] + c for c in combs)
-    combinations.extend(fill(volume, containers[1:]))
-    return combinations
+    return [c for c in powerset(containers) if sum(c) == volume]
 
 
 combinations = fill(150 if len(containers) > 5 else 25, containers)
 
-print("Part 1:", len(combinations))
+part_one(len(combinations))
 
 # Part 2
 
 comb_lengths = [len(c) for c in combinations]
 
-print("Part 2:", comb_lengths.count(min(comb_lengths)))
+part_two(comb_lengths.count(min(comb_lengths)))
