16338571d867a484b904b5c84df76df18273cbcb
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-01-07 21:41:21 +0100

    Day 25 - Christmas

--- AdventOfCode/2015/day25.py
+++ AdventOfCode/2015/day25.py
@@ -2,12 +2,25 @@ from libs import *
 
 # Parse input
 
-input = read("input")
+row, column = (
+    int(n) for n in re.match(".* row (\d*), column (\d*)\.", read("example")).groups()
+)
 
 # Part 1
 
-part_one(input)
 
-# Part 2
+def code_number(row, column):
+    n = row + column - 1
+    return (n - 1) * n // 2 + column
 
-part_two(0)
+
+def next_code(cd):
+    return (252533 * cd) % 33554393
+
+
+def code(row, column):
+    n = code_number(row, column)
+    return compose((n - 1) * [next_code], 20151125)
+
+
+part_one(code(row, column))
