c50279437808f806bcfa43d7a89282a29371bc38
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-01-07 21:45:35 +0100

    Day 12 - Introspection

--- AdventOfCode/2015/day12.py
+++ AdventOfCode/2015/day12.py
@@ -1,32 +1,44 @@
 import json
+from functools import singledispatch
+from libs import *
 
 # Parse input
 
-books = json.load(open("AdventOfCode/2015/examples/day12.in"))
+books = json.loads(read("example"))
 
 # Part 1
 
 
+@singledispatch
 def parse(books, excl=None):
-    if isinstance(books, dict):
-        if excl is not None and excl in books.values():
-            return 0
-        s = 0
-        for k, v in books.items():
-            s += parse(k, excl) + parse(v, excl)
-        return s
-    if isinstance(books, list):
-        s = 0
-        for v in books:
-            s += parse(v, excl)
-        return s
-    if isinstance(books, int):
-        return books
     return 0
 
 
-print("Part 1:", parse(books))
+@parse.register(int)
+def parse_int(books, excl=None):
+    return books
+
+
+@parse.register(list)
+def parse_list(books, excl=None):
+    s = 0
+    for v in books:
+        s += parse(v, excl)
+    return s
+
+
+@parse.register(dict)
+def parse_dict(books, excl=None):
+    if excl is not None and excl in books.values():
+        return 0
+    s = 0
+    for k, v in books.items():
+        s += parse(k, excl) + parse(v, excl)
+    return s
+
+
+part_one(parse(books))
 
 # Part 2
 
-print("Part 2:", parse(books, "red"))
+part_two(parse(books, "red"))
