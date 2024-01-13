cdc943441971a1672a61b67c1fe9c2e8417dc793
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-01-13 16:12:57 +0100

    Day 1 - Calorie Counting

--- AdventOfCode/2022/day01.js
+++ AdventOfCode/2022/day01.js
@@ -1,7 +1,11 @@
-/**
- * Day 01:
- *
- * .
- */
+const { read, sum, part_one, part_two } = require("../../common/aoc");
 
-const inputs = require("./inputs/day01");
+const elvesCalories = read("example")
+    .split("\n\n") // split per elf
+    .map((a) => a.split("\n").map((i) => parseInt(i))) // split inventory items
+    .map((a) => sum(a)) // sum calories per elf
+    .sort((a, b) => b - a);
+
+part_one(elvesCalories[0]);
+
+part_two(sum(elvesCalories.slice(0, 3)));
