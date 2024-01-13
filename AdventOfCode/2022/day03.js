ed7333a1de41d6ef7965ebe34ac0027f2f95596b
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-01-13 16:43:06 +0100

    Day 3 - Rucksack Reorganization

--- AdventOfCode/2022/day03.js
+++ AdventOfCode/2022/day03.js
@@ -1,7 +1,34 @@
-/**
- * Day 03:
- *
- * .
- */
+const { readLines, sum, part_one, part_two } = require("../../common/aoc");
 
-const inputs = require("./inputs/day03");
+const rucksacks = readLines("example");
+
+const priorities = rucksacks
+    .map((a) => [a.substring(0, a.length / 2), a.substring(a.length / 2)])
+    .map(([a, b]) => [new Set([...a]), new Set([...b])])
+    .map(([c, d]) => [...d].filter((n) => c.has(n)))
+    .map((e) => (e.length === 1 ? priority(e[0]) : -1));
+
+function priority(a) {
+    if (a >= "a") {
+        return a.charCodeAt(0) - 96;
+    } else {
+        return a.charCodeAt(0) - 38;
+    }
+}
+
+part_one(sum(priorities));
+
+const items = rucksacks.map((s) => new Set([...s]));
+
+const groups = items
+    .reduce(
+        (a, s, i) => {
+            a[Math.floor(i / 3)].push(s);
+            return a;
+        },
+        new Array(items.length / 3).fill(0).map(() => [])
+    )
+    .map(([a, b, c]) => [...a].filter((k) => b.has(k)).filter((k) => c.has(k)))
+    .map((a) => (a.length === 1 ? priority(a[0]) : -1));
+
+part_two(sum(groups));
