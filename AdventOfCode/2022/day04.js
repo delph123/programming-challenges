4287c0270bca78cb1bf1e1329d6d100d328b85c2
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-01-13 16:43:16 +0100

    Day 4 - Section overlapping

--- AdventOfCode/2022/day04.js
+++ AdventOfCode/2022/day04.js
@@ -1,7 +1,17 @@
-/**
- * Day 04:
- *
- * .
- */
+const { readLines, part_one, part_two } = require("../../common/aoc");
 
-const inputs = require("./inputs/day04");
+const pairs = readLines("example").map((a) =>
+    a.split(",").map((p) => p.split("-").map((i) => parseInt(i)))
+);
+
+function includes([a, b], [c, d]) {
+    return (a <= c && b >= d) || (c <= a && d >= b);
+}
+
+part_one(pairs.filter(([p1, p2]) => includes(p1, p2)).length);
+
+function overlap([a, b], [c, d]) {
+    return a <= d && b >= c;
+}
+
+part_two(pairs.filter(([p1, p2]) => overlap(p1, p2)).length);
