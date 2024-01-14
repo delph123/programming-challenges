ea206c13eba3cb8695a9e1ca1d869b16d6c84eee
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-01-15 00:16:36 +0100

    Day 25 - Base5 encoding

--- AdventOfCode/2022/day25.js
+++ AdventOfCode/2022/day25.js
@@ -1,7 +1,31 @@
-/**
- * Day 25:
- *
- * .
- */
+const { part_one, sum, readLines } = require("../../common/aoc");
 
-const inputs = require("./inputs/day25");
+const fuels = readLines("example");
+
+part_one(unbase5(sum(fuels.map(base5))));
+
+function decimal(d) {
+    if (d === "=") return -2;
+    if (d === "-") return -1;
+    return parseInt(d);
+}
+
+function five(d) {
+    if (d < 3) return [0, d.toString()];
+    if (d === 3) return [1, "="];
+    if (d === 4) return [1, "-"];
+}
+
+function base5(number) {
+    if (number.length === 0) return 0;
+    return (
+        5 * base5(number.slice(0, number.length - 1)) +
+        decimal(number[number.length - 1])
+    );
+}
+
+function unbase5(n) {
+    if (n === 0) return "";
+    let [r, l] = five(n % 5);
+    return unbase5(Math.floor(n / 5) + r) + l;
+}
