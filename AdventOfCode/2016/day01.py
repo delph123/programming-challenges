6c5e2bf86cd5af7168b498559a8529b3bfd64eb3
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-02-03 15:46:01 +0100

    Day 01 - Grid walk

--- AdventOfCode/2016/day01.py
+++ AdventOfCode/2016/day01.py
@@ -2,12 +2,34 @@ from libs import *
 
 # Parse input
 
-input = read("input")
+instructions = [(d[0], int(d[1:])) for d in read("example").split(", ")]
+
+DIRS = [-1j, 1, 1j, -1]
 
 # Part 1
 
-part_one(input)
+
+def walk(start, direction):
+    for turn, dist in instructions:
+        direction = (direction + (1 if turn == "R" else 3)) % 4
+        start += dist * DIRS[direction]
+    return abs(int(start.real)) + abs(int(start.imag))
+
+
+part_one(walk(0, 0))
 
 # Part 2
 
-part_two(0)
+
+def walk_p2(start, direction):
+    visited = set()
+    for turn, dist in instructions:
+        direction = (direction + (1 if turn == "R" else 3)) % 4
+        for _ in range(dist):
+            start += DIRS[direction]
+            if start in visited:
+                return abs(int(start.real)) + abs(int(start.imag))
+            visited.add(start)
+
+
+part_two(walk_p2(0, 0))
