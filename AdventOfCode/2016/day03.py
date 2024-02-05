4fcee38424700f4e487fb8962b7bb4b0f3cd85d8
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-02-05 09:06:13 +0100

    Day 3 - Triangles

--- AdventOfCode/2016/day03.py
+++ AdventOfCode/2016/day03.py
@@ -1,13 +1,34 @@
 from libs import *
+from itertools import batched
 
 # Parse input
 
-input = read("input")
+triangles = [[int(n) for n in l.split()] for l in read_lines("example")]
+
 
 # Part 1
 
-part_one(input)
+
+def is_valid(a, b, c):
+    return a + b > c
+
+
+def valid_triangles(triangles):
+    return len([l for l in triangles if is_valid(*sorted(l))])
+
+
+part_one(valid_triangles(triangles))
+
 
 # Part 2
 
-part_two(0)
+
+def pick(triangle, col):
+    return [row[col] for row in triangle]
+
+
+part_two(
+    valid_triangles(batched(pick(triangles, 0), n=3))
+    + valid_triangles(batched(pick(triangles, 1), n=3))
+    + valid_triangles(batched(pick(triangles, 2), n=3))
+)
