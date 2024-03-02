8b7db01249712d27e6fd0a4388543fc4b6c3548f
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-03-02 17:32:26 +0100

    Day 5 - Hashcash

--- AdventOfCode/2016/day05.py
+++ AdventOfCode/2016/day05.py
@@ -1,13 +1,36 @@
 from libs import *
+from hashlib import md5
 
 # Parse input
 
-input = read("input")
+door_id = read("example")
 
 # Part 1
 
-part_one(input)
+
+def password(seed: str):
+    pw = ""
+    for i in range(100000000):
+        if md5((seed + str(i)).encode()).hexdigest().startswith("00000"):
+            pw += md5((seed + str(i)).encode()).hexdigest()[5]
+            if len(pw) == 8:
+                return pw
+
+
+part_one(password(door_id))
 
 # Part 2
 
-part_two(0)
+
+def password_p2(seed: str):
+    pw = ["", "", "", "", "", "", "", ""]
+    for i in range(100000000):
+        if md5((seed + str(i)).encode()).hexdigest().startswith("00000"):
+            digest = md5((seed + str(i)).encode()).hexdigest()
+            if "0" <= digest[5] <= "7" and pw[int(digest[5])] == "":
+                pw[int(digest[5])] = digest[6]
+            if all(l != "" for l in pw):
+                return "".join(pw)
+
+
+part_two(password_p2(door_id))
