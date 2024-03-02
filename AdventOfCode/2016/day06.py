2c6ed70635d620c140e9d7ca8726ee14e0952193
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-03-02 17:43:00 +0100

    Day 6 - Least common letter

--- AdventOfCode/2016/day06.py
+++ AdventOfCode/2016/day06.py
@@ -2,12 +2,31 @@ from libs import *
 
 # Parse input
 
-input = read("input")
+messages = read_lines("example")
 
 # Part 1
 
-part_one(input)
+
+def pick(messages, col):
+    return [row[col] for row in messages]
+
+
+part_one(
+    "".join(
+        [
+            Counter(pick(messages, i)).most_common()[0][0]
+            for i in range(len(messages[0]))
+        ]
+    )
+)
 
 # Part 2
 
-part_two(0)
+part_two(
+    "".join(
+        [
+            Counter(pick(messages, i)).most_common()[-1][0]
+            for i in range(len(messages[0]))
+        ]
+    )
+)
