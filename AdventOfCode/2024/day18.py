8e416db24facb22ebfb2c8f6138ebfb1e071d361
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-12-19 22:34:57 +0100

    Day 18 - Bisecting shortest path

--- AdventOfCode/2024/day18.py
+++ AdventOfCode/2024/day18.py
@@ -2,12 +2,53 @@ from libs import *
 
 # Parse input
 
-input = read("input")
+file = "example"
+coordinates = [
+    Point(int(l.split(",")[0]), int(l.split(",")[1])) for l in read_lines(file)
+]
 
 # Part 1
 
-part_one(input)
+size = 71 if file.startswith("i") else 7
+start = Point(0, 0)
+end = Point(size - 1, size - 1)
+
+
+class MemoryAStar(AStar):
+    def __init__(self, corrupted_memory_size) -> None:
+        super().__init__()
+        self.corrupted_memory = set(coordinates[:corrupted_memory_size])
+
+    def neighbors(self, node: Point):
+        return (
+            node + d
+            for d in Point.UDLR.values()
+            if (node + d).is_within_bounds(size, size)
+            and (node + d) not in self.corrupted_memory
+        )
+
+    def cost_between(self, node1, node2):
+        return 1
+
+    def heuristic_cost_estimate(self, current, cost, goal):
+        return cost + current.manhattan(goal)
+
+
+part_one(MemoryAStar(1024 if file.startswith("i") else 12).solve(start, end).cost())
 
 # Part 2
 
-part_two(0)
+
+# Bisect coordinates set to find first coordinates that prevents reaching end
+def bad_coordinates(min, max):
+    s = MemoryAStar(min + (max - min) // 2).solve(start, end) is not None
+    if min >= max - 1:
+        return min + int(s) - 1
+    elif s:
+        return bad_coordinates(min + (max - min) // 2 + 1, max)
+    else:
+        return bad_coordinates(min, min + (max - min) // 2)
+
+
+c = bad_coordinates(1024 if file.startswith("i") else 12, len(coordinates))
+part_two(str(coordinates[c].x) + "," + str(coordinates[c].y))
