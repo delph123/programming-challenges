8b3e458e2de642217eca00ae5cd8e8f206219bec
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-12-19 22:54:49 +0100

    Day 19 - Arrangements

--- AdventOfCode/2024/day19.py
+++ AdventOfCode/2024/day19.py
@@ -2,12 +2,36 @@ from libs import *
 
 # Parse input
 
-input = read("input")
+towels, designs = read("example").split("\n\n")
+towels = towels.split(", ")
+designs = designs.split("\n")
 
 # Part 1
 
-part_one(input)
+
+def is_valid(design):
+    for towel in towels:
+        if towel == design:
+            return True
+        if design.startswith(towel) and is_valid(design[len(towel) :]):
+            return True
+    return False
+
+
+part_one(sum([int(is_valid(design)) for design in designs]))
 
 # Part 2
 
-part_two(0)
+
+@cache
+def arrangements(design):
+    a = 0
+    for towel in towels:
+        if towel == design:
+            a += 1
+        elif design.startswith(towel):
+            a += arrangements(design[len(towel) :])
+    return a
+
+
+part_two(sum([arrangements(design) for design in designs]))
