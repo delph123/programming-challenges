b95bfca9a74f2de2623c53652b4497c4bd61088c
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-12-20 17:40:17 +0100

    Day 17 - Shortest path

--- AdventOfCode/2023/day17.py
+++ AdventOfCode/2023/day17.py
@@ -2,16 +2,12 @@ from libs import *
 
 # Parse file
 
-heat_map = [[int(n) for n in l] for l in read_lines("i")]
+heat_map = read_grid("example", int)
 
-lava_pool = (0, 0)
-machine_parts = ((len(heat_map) - 1) + (len(heat_map) - 1) * 1j, 0)
+lava_pool = (heat_map.index_at(0), Point.ZERO)
+machine_parts = (heat_map.index_at(-1), Point.ZERO)
 
 # Part 1
-
-
-def get(map, coord):
-    return map[int(coord.imag)][int(coord.real)]
 
 
 class HeatMazeAStar(AStar):
@@ -19,50 +15,41 @@ class HeatMazeAStar(AStar):
         super().__init__()
         self.maze = maze
 
-    def walk(self, n, d, to):
-        s = int(abs(d.real + d.imag)) if d != 0 else 1
+    def walk(self, n: Point, d: Point, to: Point):
+        s = d.manhattan() if d != Point.ZERO else 1
         # No half turn
-        if to == -d / s:
+        if to == -d // s:
             return None
         # No more than 3 steps in straight line
-        if to == d / s and s >= 3:
+        if to == d // s and s >= 3:
             return None
         n2 = n + to
         # Not out-of-bounds
-        if n2.real < 0 or n2.real >= len(self.maze[0]):
-            return None
-        if n2.imag < 0 or n2.imag >= len(self.maze):
+        if n2 not in self.maze:
             return None
         # If same direction, count steps
-        if to == d / s:
+        if to == d // s:
             return (n2, d + to)
         else:
             return (n2, to)
 
     def neighbors(self, node):
         n, d = node
-        ns = [
-            self.walk(n, d, 1),
-            self.walk(n, d, -1),
-            self.walk(n, d, 1j),
-            self.walk(n, d, -1j),
-        ]
+        ns = [self.walk(n, d, to) for to in Point.UDLR.values()]
         return [m for m in ns if m is not None]
 
     def cost_between(self, node1, node2):
-        return get(self.maze, node2[0])
+        return self.maze[node2[0]]
 
     def heuristic_cost_estimate(self, current, cost, goal):
-        g = goal[0]
-        c = current[0]
-        return cost + int(abs(g.real - c.real)) + int(abs(g.imag - c.imag))
+        return cost + goal[0].manhattan(current[0])
 
     def is_goal_reached(self, current, goal):
         return current[0] == goal[0]
 
 
 def heat(maze, path):
-    h = [get(maze, p) for p, _ in path]
+    h = [maze[p] for p, _ in path]
     return h[1:]
 
 
@@ -75,33 +62,29 @@ part_one(
 
 class HeatMazeAStarP2(HeatMazeAStar):
     def walk(self, n, d, to):
-        s = int(abs(d.real + d.imag)) if d != 0 else 1
+        s = d.manhattan() if d != Point.ZERO else 1
         # No half turn
-        if to == -d / s:
+        if to == -d // s:
             return None
         # No more than 10 steps in straight line
-        if to == d / s and s >= 10:
+        if to == d // s and s >= 10:
             return None
         # At least 4 steps before next turn
-        if d != 0 and to != d / s and s < 4:
+        if d != Point.ZERO and to != d // s and s < 4:
             return None
         n2 = n + to
         # Not out-of-bounds
-        if n2.real < 0 or n2.real >= len(self.maze[0]):
-            return None
-        if n2.imag < 0 or n2.imag >= len(self.maze):
+        if n2 not in self.maze:
             return None
         # If same direction, count steps
-        if to == d / s:
+        if to == d // s:
             return (n2, d + to)
         else:
             return (n2, to)
 
     def is_goal_reached(self, current, goal):
         # Goal can be reach after 4 straight steps minimum
-        n, d = current
-        s = int(abs(d.real + d.imag))
-        return n == goal[0] and s >= 4
+        return current[0] == goal[0] and current[1].manhattan() >= 4
 
 
 part_two(
