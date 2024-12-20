f324bfeffbe801ab00267a65119df7fa95e00b1b
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-12-20 17:57:10 +0100

    Day 08 - Discrete Straight Line

--- AdventOfCode/2024/day08.py
+++ AdventOfCode/2024/day08.py
@@ -1,18 +1,18 @@
 from libs import *
+from operator import itemgetter
 
 # Parse input
 
-city_map = read_lines("example")
+city_map = read_grid("example")
 
 # Part 1
 
 
 def antenna_locations():
-    locations = {}
-    for j, r in enumerate(city_map):
-        for i, c in enumerate(r):
-            if c != ".":
-                locations.setdefault(c, []).append(Point(i, j))
+    locations = defaultdict(list)
+    for p, c in city_map.items():
+        if c != ".":
+            locations[c].append(p)
     return locations
 
 
@@ -20,9 +20,9 @@ def antinodes(locations: list[Point]):
     nodes = set()
     for i, l1 in enumerate(locations):
         for l2 in locations[i + 1 :]:
-            if (l1 + l1 - l2).is_within(city_map):
+            if (l1 + l1 - l2) in city_map:
                 nodes.add(l1 + l1 - l2)
-            if (l2 + l2 - l1).is_within(city_map):
+            if (l2 + l2 - l1) in city_map:
                 nodes.add(l2 + l2 - l1)
     return nodes
 
@@ -44,11 +44,11 @@ def antinodes_v2(locations: list[Point]):
     for i, l1 in enumerate(locations):
         for l2 in locations[i + 1 :]:
             a = l1 - l2
-            while (l1 + a).is_within(city_map):
+            while (l1 + a) in city_map:
                 nodes.add(l1 + a)
                 a += l1 - l2
             a = l1 - l2
-            while (l2 - a).is_within(city_map):
+            while (l2 - a) in city_map:
                 nodes.add(l2 - a)
                 a += l1 - l2
     return nodes
