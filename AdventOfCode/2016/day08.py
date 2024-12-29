a9276a9d4853d0b7d148deed9ecaa8ff6a242348
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-12-29 22:48:30 +0100

    Day 08 - LCD display

--- AdventOfCode/2016/day08.py
+++ AdventOfCode/2016/day08.py
@@ -2,12 +2,45 @@ from libs import *
 
 # Parse input
 
-input = read("input")
+file = "example"
+instructions = read_lines(file)
 
 # Part 1
 
-part_one(input)
+
+def parse(instruction):
+    if instruction.startswith("rect"):
+        return ("rect", tuple(int(d) for d in instruction[5:].split("x")))
+    elif instruction.startswith("rotate row"):
+        return ("rotr", tuple(int(d) for d in instruction[13:].split(" by ")))
+    elif instruction.startswith("rotate column"):
+        return ("rotc", tuple(int(d) for d in instruction[16:].split(" by ")))
+
+
+def display(instructions, width, height):
+    screen = Grid.of_size(width, height).fill(".")
+    for instruction in instructions:
+        (op, (a, b)) = parse(instruction)
+        if op == "rect":
+            for x in range(a):
+                for y in range(b):
+                    screen[Point(x, y)] = "#"
+        elif op == "rotc":
+            col = [screen[Point(a, y)] for y in range(height)]
+            for y in range(height):
+                screen[Point(a, y)] = col[(y - b) % height]
+        elif op == "rotr":
+            row = [screen[Point(x, a)] for x in range(width)]
+            for x in range(width):
+                screen[Point(x, a)] = row[(x - b) % width]
+    return screen
+
+
+width, height = (50, 6) if file.startswith("i") else (7, 3)
+screen = display(instructions, width, height)
+
+part_one(screen.count("#"))
 
 # Part 2
 
-part_two(0)
+part_two(screen)
