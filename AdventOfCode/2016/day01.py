f6b9683816824cc25541fb88e209e59230e54e83
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-12-29 23:03:22 +0100

    Day 01 - Grid walk

--- AdventOfCode/2016/day01.py
+++ AdventOfCode/2016/day01.py
@@ -4,32 +4,30 @@ from libs import *
 
 instructions = [(d[0], int(d[1:])) for d in read("example").split(", ")]
 
-DIRS = [-1j, 1, 1j, -1]
-
 # Part 1
 
 
-def walk(start, direction):
+def walk(start: Point, direction: Point):
     for turn, dist in instructions:
-        direction = (direction + (1 if turn == "R" else 3)) % 4
-        start += dist * DIRS[direction]
-    return abs(int(start.real)) + abs(int(start.imag))
+        direction = direction.rotate(90 if turn == "R" else -90)
+        start += dist * direction
+    return start.manhattan()
 
 
-part_one(walk(0, 0))
+part_one(walk(Point.ZERO, Point.UDLR["^"]))
 
 # Part 2
 
 
-def walk_p2(start, direction):
+def walk_p2(start: Point, direction: Point):
     visited = set()
     for turn, dist in instructions:
-        direction = (direction + (1 if turn == "R" else 3)) % 4
+        direction = direction.rotate(90 if turn == "R" else -90)
         for _ in range(dist):
-            start += DIRS[direction]
+            start += direction
             if start in visited:
-                return abs(int(start.real)) + abs(int(start.imag))
+                return start.manhattan()
             visited.add(start)
 
 
-part_two(walk_p2(0, 0))
+part_two(walk_p2(Point.ZERO, Point.UDLR["^"]))
