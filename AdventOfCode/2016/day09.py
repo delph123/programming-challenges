c4faff3c38ea1cf17ccaf3dcc491e39eb84e8f20
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2024-12-30 11:29:40 +0100

    Day 09 - Recursive string unpacking

--- AdventOfCode/2016/day09.py
+++ AdventOfCode/2016/day09.py
@@ -2,12 +2,43 @@ from libs import *
 
 # Parse input
 
-input = read("input")
+compressed = read_lines("example", ignore=[" "])
 
 # Part 1
 
-part_one(input)
+
+def decompressed(seq):
+    pattern = re.compile(r"\((\d+)x(\d+)\)")
+    out = ""
+    n = 0
+    m = pattern.search(seq, n)
+    while m is not None:
+        length, times = (int(m.group(1)), int(m.group(2)))
+        out += seq[n : m.start()] + seq[m.end() : m.end() + length] * times
+        n = m.end() + length
+        m = pattern.search(seq, n)
+    out += seq[n:]
+    return out
+
+
+part_one([len(decompressed(l)) for l in compressed])
 
 # Part 2
 
-part_two(0)
+
+def len_decompressed(seq):
+    pattern = re.compile(r"\((\d+)x(\d+)\)")
+    total_length = 0
+    n = 0
+    m = pattern.search(seq, n)
+    while m is not None:
+        length, times = (int(m.group(1)), int(m.group(2)))
+        total_length += m.start() - n
+        total_length += times * len_decompressed(seq[m.end() : m.end() + length])
+        n = m.end() + length
+        m = pattern.search(seq, n)
+    total_length += len(seq) - n
+    return total_length
+
+
+part_two([len_decompressed(l) for l in compressed])
