f55b3669a1ac277af1387dac809f9ae1fc1925fb
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-01-02 18:37:43 +0100

    Day 16 - Binary dragon curve

--- AdventOfCode/2016/day16.py
+++ AdventOfCode/2016/day16.py
@@ -2,12 +2,30 @@ from libs import *
 
 # Parse input
 
-input = read("input")
+initial_state, disk_length = read_lines("example")
+disk_length = int(disk_length)
 
 # Part 1
 
-part_one(input)
+
+def dragon_curve(state: str, size):
+    while len(state) < size:
+        b = int("".join(reversed(state)), 2)
+        b ^= 2 ** len(state) - 1
+        state += "0" + format(b, f"0{len(state)}b")
+    return state[:size]
+
+
+def checksum(data):
+    cs = "".join([str(int(a == b)) for a, b in batched(data, 2)])
+    if len(cs) % 2 == 0:
+        return checksum(cs)
+    else:
+        return cs
+
+
+part_one(checksum(dragon_curve(initial_state, disk_length)))
 
 # Part 2
 
-part_two(0)
+part_two(checksum(dragon_curve(initial_state, 35651584)))
