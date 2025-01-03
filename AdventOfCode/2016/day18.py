e65d6d15be64aa47ee5dfda93dee32483c2856df
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-01-03 16:17:48 +0100

    Day 18 - Paving the floor

--- AdventOfCode/2016/day18.py
+++ AdventOfCode/2016/day18.py
@@ -2,12 +2,36 @@ from libs import *
 
 # Parse input
 
-input = read("input")
+tiles = read("example")
+
+grid_rows = 10 if read.from_example else 40
 
 # Part 1
 
-part_one(input)
+
+def next_row(row):
+    next = []
+    row = "." + row + "."
+    for i in range(1, len(row) - 1):
+        next.append("^" if row[i - 1 : i + 2 : 2] in ["^.", ".^"] else ".")
+    return "".join(next)
+
+
+part_one(
+    Grid(
+        reduce(lambda a, _: a + [next_row(a[-1])], range(1, grid_rows), [tiles])
+    ).count(".")
+)
 
 # Part 2
 
-part_two(0)
+
+def count_tiles(t, row, size):
+    c = 0
+    for _ in range(size):
+        c += row.count(t)
+        row = next_row(row)
+    return c
+
+
+part_two(count_tiles(".", tiles, 400000))
