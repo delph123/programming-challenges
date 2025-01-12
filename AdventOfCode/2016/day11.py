493571c634d2ab15a2bb10c23a80b1517140295d
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-01-13 00:05:19 +0100

    Day 11 - Dangerous crossing (modified river crossing puzzle)
    Optimization of runtime from 10 minutes to 2 seconds by pruning equivalent states.

--- AdventOfCode/2016/day11.py
+++ AdventOfCode/2016/day11.py
@@ -1,20 +1,29 @@
 from libs import *
+from collections import namedtuple
+
+Parts = namedtuple("Floor", ["rtgs", "chips"])
+
 
 # Parse input
 
 FLOORS = ["fourth", "third", "second", "first"]
 
 floors_content = read_lines(
-    "i",
+    "example",
     ignore=["The ", "a ", "floor contains ", "and ", ",", ".", " nothing relevant"],
-    replace=[(" generator", "-gen"), ("-compatible microchip", "-chip")],
+    replace=[(" generator", "-rtg"), ("-compatible microchip", "-chip")],
 )
 
 floors_content = sorted(
     [c.split(" ") for c in floors_content], key=lambda f: FLOORS.index(f[0])
 )
 floors_content = tuple(
-    tuple(sorted(tuple(reversed(el.split("-"))) for el in elements[1:]))
+    Parts(
+        frozenset(el.split("-")[0] for el in elements[1:] if el.split("-")[1] == "rtg"),
+        frozenset(
+            el.split("-")[0] for el in elements[1:] if el.split("-")[1] == "chip"
+        ),
+    )
     for elements in floors_content
 )
 
@@ -25,26 +34,24 @@ target_floor = FLOORS.index("fourth")
 
 
 def print_floors(e, floors):
-    inc = int(copysign(1, elevator_floor - target_floor))
-    n = len(FLOORS)
+    n, inc = len(floors), int(copysign(1, elevator_floor - target_floor))
     for i in range(target_floor, elevator_floor + inc, inc):
-        print(f"F{n}", "E" if e == i else " ", end=" ")
-        print(*sorted(f"{b}-{a}" for a, b in floors[i]), sep=" ")
+        connected = floors[i].rtgs & floors[i].chips
+        rtgs = floors[i].rtgs - floors[i].chips
+        chips = floors[i].chips - floors[i].rtgs
+        print(f"\x1b[33m{n}\x1b[0m", "\x1b[1;35mE\x1b[0m" if e == i else " ", end=" ")
+        if connected:
+            print("\x1b[1;31mConnected = \x1b[0m", end="")
+            print(*sorted(e for e in connected), sep=", ", end=" ")
+        if rtgs:
+            print("\x1b[1;32mRTGs = \x1b[0m", end="")
+            print(*sorted(e for e in rtgs), sep=", ", end=" ")
+        if chips:
+            print("\x1b[1;34mChips = \x1b[0m", end="")
+            print(*sorted(e for e in chips), sep=", ", end="")
+        print()
         n -= 1
     print()
-
-
-def valid_floor(content):
-    floor_content = {
-        t: set(x for (_, x) in c) for t, c in groupby(content, key=itemgetter(0))
-    }
-    gens = floor_content.get("gen", set())
-    chips = floor_content.get("chip", set())
-    return len(chips - gens) == 0 or len(gens) == 0
-
-
-def is_valid(floors):
-    return all(valid_floor(f) for f in floors)
 
 
 class FloorAStar(AStar):
@@ -55,46 +62,61 @@ class FloorAStar(AStar):
         e, floors = current
         h = cost
         nb = 0
-        malus = False
         inc = int(copysign(1, target_floor - elevator_floor))
         for i in range(elevator_floor, target_floor, inc):
-            nb += len(floors[i])
-            if not malus and nb > 0:
-                malus = True
-                h += abs(e - i)
-                if abs(target_floor - e) < abs(target_floor - i):
-                    h += 1
+            nb += len(floors[i].rtgs) + len(floors[i].chips)
             h += 1 if nb <= 2 else nb
+        l = self.min_filled_floor(floors)
+        if l != e:
+            h += abs(e - l) + 1
         return h
 
     def neighbors(self, current):
         e, floors = current
-        if e + 1 < len(floors):
-            for swaps in chain(combinations(floors[e], 2), combinations(floors[e], 1)):
-                np = self.position(floors, e, swaps, e + 1)
-                if is_valid(np):
-                    yield ((e + 1), np)
-        if e > 0:
-            for swaps in chain(combinations(floors[e], 2), combinations(floors[e], 1)):
-                np = self.position(floors, e, swaps, e - 1)
-                if is_valid(np):
-                    yield ((e - 1), np)
+        parts = self.expand(floors[e])
+        for swaps in chain(combinations(parts, 2), combinations(parts, 1)):
+            for d in (d for d in [-1, 1] if 0 <= e + d < len(floors)):
+                np = self.position(floors, e, swaps, e + d)
+                if self.is_valid(np):
+                    yield ((e + d), np)
+
+    def key(self, current):
+        e, floors = current
+        elements = defaultdict(int)
+        for i, floor in enumerate(floors):
+            for n in floor.rtgs:
+                elements[n] += 10 * (i + 1)
+            for n in floor.chips:
+                elements[n] += i + 1
+        return (e, tuple(sorted(elements.values())))
 
     def is_goal_reached(self, current, goal=None):
-        return all(i == target_floor or len(c) == 0 for i, c in enumerate(current[1]))
+        return self.min_filled_floor(current[1]) == target_floor
+
+    def min_filled_floor(self, floors):
+        inc = int(copysign(1, target_floor - elevator_floor))
+        for i in range(elevator_floor, target_floor + inc, inc):
+            if floors[i].rtgs or floors[i].chips:
+                return i
+
+    def is_valid(self, floors):
+        return all(not f.rtgs or f.chips <= f.rtgs for f in floors)
+
+    def expand(self, floor_content):
+        candidates = [("rtg", n) for n in floor_content.rtgs]
+        candidates.extend([("chip", n) for n in floor_content.chips])
+        return candidates
 
     def position(self, floors, level, swaps, next_level):
+        rswap = frozenset(n for (k, n) in swaps if k == "rtg")
+        cswap = frozenset(n for (k, n) in swaps if k == "chip")
         return tuple(
             (
-                elements
-                if i not in (level, next_level)
-                else (
-                    tuple(sorted(set(elements) - set(swaps)))
-                    if i == level
-                    else tuple(sorted(set(elements) | set(swaps)))
-                )
+                Parts(f.rtgs - rswap, f.chips - cswap)
+                if i == level
+                else (Parts(f.rtgs | rswap, f.chips | cswap) if i == next_level else f)
             )
-            for i, elements in enumerate(floors)
+            for i, f in enumerate(floors)
         )
 
 
@@ -107,17 +129,12 @@ part_one(sol_p1.cost())
 
 # Part 2
 
-new_elements = product(["gen", "chip"], ["elerium", "dilithium"])
+new_elements = {"elerium", "dilithium"}
+to_floor = 1 if read.from_example else elevator_floor
 floors_content_p2 = tuple(
-    (
-        tuple(sorted(set(elements) | set(new_elements)))
-        if i == elevator_floor
-        else elements
-    )
-    for i, elements in enumerate(floors_content)
+    (Parts(f.rtgs | new_elements, f.chips | new_elements) if i == to_floor else f)
+    for i, f in enumerate(floors_content)
 )
-
-# Part 2 takes very long to compute but eventually finishes.
 
 sol_p2 = FloorAStar().solve((elevator_floor, floors_content_p2))
 
