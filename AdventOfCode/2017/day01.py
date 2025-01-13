f09a4bd4f071b4c9f27afd0b027c950eee202675
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-01-13 21:30:05 +0100

    Day 01 - Inverse CAPTCHA

--- AdventOfCode/2017/day01.py
+++ AdventOfCode/2017/day01.py
@@ -2,12 +2,23 @@ from libs import *
 
 # Parse input
 
-data = read("input")
+digits_sequences = [[int(n) for n in l] for l in read_lines("example")]
 
 # Part 1
 
-part_one(data)
+
+def captcha(seq):
+    return sum([p for (p, q) in pairwise(seq + [seq[0]]) if p == q])
+
+
+part_one([captcha(d) for d in digits_sequences[:4]])
 
 # Part 2
 
-part_two(0)
+
+def captcha_p2(seq):
+    l = len(seq)
+    return sum([d for i, d in enumerate(seq) if d == seq[(i + l // 2) % l]])
+
+
+part_two([captcha_p2(d) for d in digits_sequences[-5:]])
