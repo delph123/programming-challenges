72c887394516dd40cfbfd8f5d2424dfb0f39c961
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-01-13 22:02:59 +0100

    Day 02 - Checksum

--- AdventOfCode/2017/day02.py
+++ AdventOfCode/2017/day02.py
@@ -2,12 +2,27 @@ from libs import *
 
 # Parse input
 
-data = read("input")
+spreadsheet = [tuple(int(d) for d in l.split()) for l in read_lines("example")]
+
+sheet1 = spreadsheet[: len(spreadsheet) // 2] if read.from_example else spreadsheet
+sheet2 = spreadsheet[-(len(spreadsheet) // 2) :] if read.from_example else spreadsheet
 
 # Part 1
 
-part_one(data)
+
+def checksum(spreadsheet):
+    return sum(max(l) - min(l) for l in spreadsheet)
+
+
+part_one(checksum(sheet1))
 
 # Part 2
 
-part_two(0)
+
+def multiples(line):
+    for a, b in combinations(line, 2):
+        if max(a, b) % min(a, b) == 0:
+            return max(a, b) // min(a, b)
+
+
+part_two(sum(multiples(l) for l in sheet2))
