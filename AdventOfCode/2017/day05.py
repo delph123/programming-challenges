668d8323a65d2c721a98d9d59c3c6be16aba9171
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-01-14 22:46:09 +0100

    Day 05 - Kangaroo

--- AdventOfCode/2017/day05.py
+++ AdventOfCode/2017/day05.py
@@ -2,12 +2,32 @@ from libs import *
 
 # Parse input
 
-data = read("input")
+jumps_maze = [int(j) for j in read_lines("example")]
 
 # Part 1
 
-part_one(data)
+
+def escape(maze):
+    pc = 0
+    steps = 0
+    while 0 <= pc < len(maze):
+        maze[pc], pc = (maze[pc] + 1, pc + maze[pc])
+        steps += 1
+    return steps
+
+
+part_one(escape(deepcopy(jumps_maze)))
 
 # Part 2
 
-part_two(0)
+
+def escape_p2(maze):
+    pc = 0
+    steps = 0
+    while 0 <= pc < len(maze):
+        maze[pc], pc = (maze[pc] + (-1 if maze[pc] >= 3 else 1), pc + maze[pc])
+        steps += 1
+    return steps
+
+
+part_two(escape_p2(deepcopy(jumps_maze)))
