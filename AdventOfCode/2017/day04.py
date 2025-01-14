4aa9cdf68ea002219654e19b22d4d24a28ab3776
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-01-14 22:28:15 +0100

    Day 04 - Anagrams

--- AdventOfCode/2017/day04.py
+++ AdventOfCode/2017/day04.py
@@ -2,12 +2,27 @@ from libs import *
 
 # Parse input
 
-data = read("input")
+passphrases = read_lines("example")
+
+cut = passphrases.index("") if read.from_example else len(passphrases)
+cut_p2 = cut + 1 if read.from_example else 0
 
 # Part 1
 
-part_one(data)
+
+def valid(passphrase):
+    words = passphrase.split(" ")
+    return len(words) == len(set(words))
+
+
+part_one(sum([valid(phrase) for phrase in passphrases[:cut]]))
 
 # Part 2
 
-part_two(0)
+
+def valid_p2(passphrase):
+    words = ["".join(sorted(word)) for word in passphrase.split(" ")]
+    return len(words) == len(set(words))
+
+
+part_two(sum([valid_p2(phrase) for phrase in passphrases[cut_p2:]]))
