d0b03d1034121154a4b18c373e57988a729a5427
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-01-15 22:41:45 +0100

    Day 08 - Fused add-condition

--- AdventOfCode/2017/day08.py
+++ AdventOfCode/2017/day08.py
@@ -1,13 +1,37 @@
 from libs import *
+from operator import lt, gt, le, ge, eq, ne
 
 # Parse input
 
-data = read("input")
+instructions = [l.split() for l in read_lines("e")]
+instructions = [(a, b, int(c), d, e, int(f)) for (a, b, c, _, d, e, f) in instructions]
+
+operations = {"<": lt, "<=": le, ">": gt, ">=": ge, "==": eq, "!=": ne}
 
 # Part 1
 
-part_one(data)
+
+def run(instructions):
+    registers = defaultdict(int)
+    for r, op, v, cr, cop, cv in instructions:
+        if operations[cop](registers[cr], cv):
+            registers[r] += v if op == "inc" else -v
+    return registers
+
+
+part_one(max(run(instructions).values()))
 
 # Part 2
 
-part_two(0)
+
+def run_p2(instructions):
+    registers = defaultdict(int)
+    highest = 0
+    for r, op, v, cr, cop, cv in instructions:
+        if operations[cop](registers[cr], cv):
+            registers[r] += v if op == "inc" else -v
+            highest = max(registers[r], highest)
+    return highest
+
+
+part_two(run_p2(instructions))
