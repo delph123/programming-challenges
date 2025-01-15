7f1b36c60bcbd344a44758156b0456a6ee875334
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-01-15 23:16:44 +0100

    Day 09 - Matching braces

--- AdventOfCode/2017/day09.py
+++ AdventOfCode/2017/day09.py
@@ -2,12 +2,54 @@ from libs import *
 
 # Parse input
 
-data = read("input")
+streams = read_lines("example")
 
 # Part 1
 
-part_one(data)
+
+def score(stream):
+    opened = 0
+    s = 0
+    garbage = False
+    ignore = False
+    for c in stream:
+        if ignore:
+            ignore = False
+        elif c == "!":
+            ignore = True
+        elif garbage and c == ">":
+            garbage = False
+        elif not garbage and c == "{":
+            opened += 1
+            s += opened
+        elif not garbage and c == "}":
+            opened -= 1
+        elif c == "<":
+            garbage = True
+    return s
+
+
+part_one([score(stream) for stream in streams[:8]], sep=", ")
 
 # Part 2
 
-part_two(0)
+
+def garbage(stream):
+    s = 0
+    garbage = False
+    ignore = False
+    for c in stream:
+        if ignore:
+            ignore = False
+        elif c == "!":
+            ignore = True
+        elif garbage and c == ">":
+            garbage = False
+        elif not garbage and c == "<":
+            garbage = True
+        elif garbage:
+            s += 1
+    return s
+
+
+part_two([garbage(stream) for stream in streams[-7:]], sep=", ")
