f795033494ce0b53b66761b225e4b1688707352e
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-01-17 23:09:45 +0100

    Day 13 - Composing oscillator

--- AdventOfCode/2017/day13.py
+++ AdventOfCode/2017/day13.py
@@ -2,12 +2,30 @@ from libs import *
 
 # Parse input
 
-data = read("input")
+firewall = [l.split(": ") for l in read_lines("example")]
+firewall = {int(a): int(b) for a, b in firewall}
 
 # Part 1
 
-part_one(data)
+
+def caught(d, r):
+    return d % (2 * (r - 1)) == 0
+
+
+def severity(firewall):
+    return sum(d * r for d, r in firewall.items() if caught(d, r))
+
+
+part_one(severity(firewall))
 
 # Part 2
 
-part_two(0)
+
+# Brute-force, no smart computation this time
+def delay(firewall):
+    return next(
+        n for n in count() if all(not caught(d + n, r) for d, r in firewall.items())
+    )
+
+
+part_two(delay(firewall))
