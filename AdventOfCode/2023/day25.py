c6e4303bdb58f4827417cea86e7e5439113941f6
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-01-19 00:29:11 +0100

    Day 25 - Splitting a graph

--- AdventOfCode/2023/day25.py
+++ AdventOfCode/2023/day25.py
@@ -1,69 +1,27 @@
-from collections import deque
-from copy import deepcopy
+from libs import *
 
 # Parse file
 
-connections = {
-    l.split(": ")[0]: l.split(": ")[1].split()
-    for l in open("AdventOfCode/2023/examples/day25.in").read().strip().split("\n")
-}
+connections_graph = Graph.undirected(
+    {l.split(": ")[0]: set(l.split(": ")[1].split()) for l in read_lines("i")}
+)
 
 
 # Part 1
 
 
-def mirror_connections(graph):
-    vertices = set(graph.keys()) | set(sum(graph.values(), []))
-    mirror = {e: set() for e in vertices}
-    for e, it in graph.items():
-        mirror[e] = mirror[e] | set(it)
-        for i in it:
-            mirror[i].add(e)
-    return mirror
-
-
-def groups(graph):
-    vertices = set(graph.keys())
-    groups = []
-    while vertices:
-        e = vertices.pop()
-        visited = {e}
-        next = {e}
-        while next:
-            n = next.pop()
-            for neigh in graph[n]:
-                if neigh not in visited:
-                    vertices.remove(neigh)
-                    visited.add(neigh)
-                    next.add(neigh)
-        groups.append(visited)
-    return groups
-
-
-def rem(graph, edges):
-    g2 = deepcopy(graph)
-    for n1, n2 in edges:
-        g2[n1].remove(n2)
-        g2[n2].remove(n1)
-    for d in [n for n in g2 if len(g2[n]) == 0]:
-        del g2[d]
-    return g2
-
-
-def cut_edges(starts, targets, graph):
+def edges_between(starts, targets, graph):
     edges = []
     for s in starts:
         edges += [(s, e) for e in graph[s] if e in targets]
     return edges
 
 
-def cut(graph, excl):
+def cut(graph: Graph, excl):
     out = set()
     g = graph
-    while len(out) == 0 or (
-        len(g.keys()) > 0 and len(cut_edges(g.keys(), out, graph)) > 3
-    ):
-        priorities = sorted(list(g.keys()), key=lambda n: len(g[n]))
+    while len(out) == 0 or (len(g) > 0 and len(edges_between(g, out, graph)) > 3):
+        priorities = sorted(g.nodes(), key=lambda n: len(g[n]))
         priority = 0
         n = priorities[priority]
         while len(out) == 0 and n in excl:
@@ -72,10 +30,10 @@ def cut(graph, excl):
         if len(out) == 0:
             excl.append(n)
         out.add(n)
-        g = rem(g, [(o1, n) for o1 in g[n]])
+        g = g.with_removed([(o1, n) for o1 in g[n]])
 
     # Check answer & compute group sizes
-    grs = groups(rem(graph, cut_edges(g.keys(), out, graph)))
+    grs = graph.with_removed(edges_between(g, out, graph)).groups()
     if len(grs) == 2:
         return len(grs[0]) * len(grs[1])
 
@@ -83,4 +41,4 @@ def cut(graph, excl):
     return cut(graph, excl)
 
 
-print("Part 1:", cut(mirror_connections(connections), []))
+part_one(cut(connections_graph, []))
--- lib/graph.py
+++ lib/graph.py
@@ -1,4 +1,5 @@
-from functools import partial
+from typing import TypeVar, Generic
+from copy import deepcopy
 
 
 def adjacent(start, neighbors):
@@ -19,45 +20,70 @@ def group_adjacent(iterable, neighbors):
             visited |= group
 
 
-class Graph:
+V = TypeVar("V")
 
-    def __init__(self, edges, oriented=False):
-        self.neighbors = {}
+
+class Graph(Generic[V]):
+
+    def __init__(self, neighbors_map: dict[V, set[V]]):
+        super().__init__()
+        self.neighbors_map = neighbors_map
+
+    @classmethod
+    def undirected(cls, neighbors_map: dict[V, set[V]]):
+        g = cls(deepcopy(neighbors_map))
+        for start, nb in neighbors_map.items():
+            for end in nb:
+                g.neighbors_map.setdefault(end, set()).add(start)
+        return g
+
+    @classmethod
+    def from_edges(cls, edges, oriented=False):
+        neighbors_map = {}
         for a, b in edges:
-            self.neighbors.setdefault(a, set())
-            self.neighbors.setdefault(b, set())
-            self.neighbors[a].add(b)
+            neighbors_map.setdefault(a, set())
+            neighbors_map.setdefault(b, set())
+            neighbors_map[a].add(b)
             if not oriented:
-                self.neighbors[b].add(a)
-
-    def neighbor(self, n, default=None):
-        return self.neighbors.get(n, default)
+                neighbors_map[b].add(a)
+        return cls(neighbors_map)
 
     def __len__(self):
-        return len(self.neighbors)
+        return len(self.neighbors_map)
+
+    def neighbors(self, n, default=None):
+        return self.neighbors_map.get(n, default)
 
     def __contains__(self, e):
         if isinstance(e, tuple):
             (a, b) = e
-            return a in self.neighbors and b in self.neighbors[a]
+            return a in self.neighbors_map and b in self.neighbors_map[a]
         else:
-            e in self.neighbors
+            e in self.neighbors_map
 
     def __getitem__(self, n):
-        return self.neighbor[n]
+        return self.neighbors_map[n]
 
     def nodes(self):
-        return set(self.neighbors.keys())
+        return set(self.neighbors_map.keys())
+
+    vertices = nodes
 
     def edges(self):
-        for start, nb in self.neighbors.items():
+        for start, nb in self.neighbors_map.items():
             for end in nb:
                 yield (start, end)
+
+    def __iter__(self):
+        return iter(self.neighbors_map)
+
+    def copy(self):
+        return Graph(deepcopy(self.neighbors_map))
 
     def _pivoted(self, candidates, excluded):
         if candidates:
             return candidates - max(
-                (candidates & self.neighbor(c) for c in (candidates | excluded)),
+                (candidates & self.neighbors(c) for c in (candidates | excluded)),
                 key=len,
             )
         else:
@@ -67,7 +93,7 @@ class Graph:
         if not candidates and not excluded:
             yield clique
         for c in self._pivoted(candidates, excluded):
-            nc = self.neighbor(c, set())
+            nc = self.neighbors(c, set())
             yield from self._bron_kerbosch(clique | {c}, candidates & nc, excluded & nc)
             candidates.remove(c)
             excluded.add(c)
@@ -79,7 +105,17 @@ class Graph:
         return max(self._bron_kerbosch(set(), self.nodes(), set()), key=len)
 
     def accessible_from(self, start):
-        return adjacent(start, self.neighbor)
+        return adjacent(start, self.neighbors)
 
     def groups(self):
-        return list(group_adjacent(self.nodes(), self.neighbor))
+        return list(group_adjacent(self, self.neighbors))
+
+    def with_removed(self, edges, oriented=False):
+        g2 = self.copy()
+        for n1, n2 in edges:
+            g2[n1].remove(n2)
+            if not oriented:
+                g2[n2].remove(n1)
+        for d in [v for v in g2 if len(g2[v]) == 0]:
+            del g2.neighbors_map[d]
+        return g2
