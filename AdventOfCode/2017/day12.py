a8cf3cb7bf6dc9aa0b98140e12f8bbcef78cd2f5
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-01-19 00:29:54 +0100

    Day 12 - Splitting graph

--- AdventOfCode/2017/day12.py
+++ AdventOfCode/2017/day12.py
@@ -3,14 +3,13 @@ from libs import *
 # Parse input
 
 pipes = [l.split(" <-> ") for l in read_lines("example")]
-pipes = {int(a): set(int(d) for d in b.split(", ")) for a, b in pipes}
 
-pipe_graph = Graph((a, b) for a, n in pipes.items() for b in n)
+pipes_graph = Graph({int(a): set(int(d) for d in b.split(", ")) for a, b in pipes})
 
 # Part 1
 
-part_one(len(pipe_graph.accessible_from(0)))
+part_one(len(pipes_graph.accessible_from(0)))
 
 # Part 2
 
-part_two(len(pipe_graph.groups()))
+part_two(len(pipes_graph.groups()))
