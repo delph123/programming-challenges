b8d98ba00f8c57db393964663a50e8c0a85e8d5e
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-01-19 16:31:03 +0100

    Day 15 - Linear Congruential Generator

--- AdventOfCode/2017/day15.py
+++ AdventOfCode/2017/day15.py
@@ -2,12 +2,48 @@ from libs import *
 
 # Parse input
 
-data = read("input")
+matcher = create_matcher([("Generator {str} starts with {int}", (0, 1))])
+
+generators_seed = [matcher(r) for r in read_lines("example")]
+generators_seed = {g: s for g, s in generators_seed}
+
+factors = {"A": 16807, "B": 48271}
+remainders = {"A": 4, "B": 8}
+
+judge_remainder = 2**16
 
 # Part 1
 
-part_one(data)
+
+def generator(name):
+    a = generators_seed[name]
+    f = factors[name]
+    while True:
+        a = (a * f) % 2147483647
+        yield a
+
+
+def matching_judge(gen, number_of_pairs):
+    return sum(
+        1
+        for a, b in islice(zip(gen("A"), gen("B")), number_of_pairs)
+        if a % judge_remainder == b % judge_remainder
+    )
+
+
+part_one(matching_judge(generator, 40_000_000))
 
 # Part 2
 
-part_two(0)
+
+def generator_p2(name):
+    a = generators_seed[name]
+    f = factors[name]
+    r = remainders[name]
+    while True:
+        a = (a * f) % 2147483647
+        if a % r == 0:
+            yield a
+
+
+part_two(matching_judge(generator_p2, 5_000_000))
