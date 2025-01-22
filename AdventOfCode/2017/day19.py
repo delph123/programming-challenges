c01a7e7b4221b7b88a999ef8630ec99471b9f64c
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-01-22 22:14:50 +0100

    Day 19 - Follow the line

--- AdventOfCode/2017/day19.py
+++ AdventOfCode/2017/day19.py
@@ -2,12 +2,46 @@ from libs import *
 
 # Parse input
 
-data = read("input")
+routing_diagram = read_grid("example")
 
 # Part 1
 
-part_one(data)
+
+def path(g: Grid):
+    s = Point(g.rows()[0].index("|"), 0)
+    p = [s]
+    d = Point.UDLR["v"]
+    while True:
+        # Advance to the '+'
+        while g.get(s + d, " ") not in "+ ":
+            s += d
+            p.append(s)
+        if g.get(s + d, " ") != " ":
+            s += d
+            p.append(s)
+        else:
+            break
+        # Turn if possible
+        if g.get(s + d.rotate(90), " ") != " ":
+            d = d.rotate(90)
+        elif g.get(s + d.rotate(-90), " ") != " ":
+            d = d.rotate(-90)
+        else:
+            break
+        s += d
+        p.append(s)
+    return p
+
+
+part_one(
+    [
+        routing_diagram[p]
+        for p in path(routing_diagram)
+        if routing_diagram[p] not in "+|-"
+    ],
+    sep="",
+)
 
 # Part 2
 
-part_two(0)
+part_two(len(path(routing_diagram)))
