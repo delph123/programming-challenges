17c6cbfead76c43fd01c76611951d578e4b53073
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-01-26 22:34:00 +0100

    Day 24 - Dominos

--- AdventOfCode/2017/day24.py
+++ AdventOfCode/2017/day24.py
@@ -2,12 +2,38 @@ from libs import *
 
 # Parse input
 
-data = read("input")
+components_ports = [tuple(int(n) for n in r.split("/")) for r in read_lines("example")]
 
 # Part 1
 
-part_one(data)
+
+def strongest(components, start):
+    max_strength = 0
+    for i, (a, b) in enumerate(components):
+        if a == start:
+            strength = strongest(components[:i] + components[i + 1 :], b)
+            max_strength = max(max_strength, a + b + strength)
+        if b == start and b != a:
+            strength = strongest(components[:i] + components[i + 1 :], a)
+            max_strength = max(max_strength, a + b + strength)
+    return max_strength
+
+
+part_one(strongest(components_ports, 0))
 
 # Part 2
 
-part_two(0)
+
+def strongest_p2(components, start):
+    max_strength = (0, 0)
+    for i, (a, b) in enumerate(components):
+        if a == start:
+            (size, strength) = strongest_p2(components[:i] + components[i + 1 :], b)
+            max_strength = max(max_strength, (size + 1, strength + a + b))
+        if b == start and b != a:
+            (size, strength) = strongest_p2(components[:i] + components[i + 1 :], a)
+            max_strength = max(max_strength, (size + 1, strength + a + b))
+    return max_strength
+
+
+part_two(strongest_p2(components_ports, 0)[1])
