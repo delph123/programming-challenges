6ff0eb6c95ae99e95f6f9a2897b7b83215c5896d
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-12-03 23:52:39 +0100

    Day 01 - Modulus rotations

--- AdventOfCode/2025/day01.py
+++ AdventOfCode/2025/day01.py
@@ -2,14 +2,37 @@ from libs import *
 
 # Parse input
 
-example = read("example")
+rotations = read_lines("example")
 
 # Part 1
 
 
-part_one(example)
+def rotate(instructions):
+    dial = 50
+    password = 0
+    for instr in instructions:
+        dial = (dial + (-1 if instr[0] == "L" else 1) * int(instr[1:])) % 100
+        if dial == 0:
+            password += 1
+    return password
+
+
+part_one(rotate(rotations))
 
 # Part 2
 
 
-part_two(0)
+def rotate_p2(instructions):
+    dial = 50
+    password = 0
+    for instr in instructions:
+        if instr[0] == "R":
+            password += (dial + int(instr[1:])) // 100
+        else:
+            password += abs((dial - int(instr[1:]) - 1) // 100)
+            password -= 1 if dial == 0 else 0
+        dial = (dial + (-1 if instr[0] == "L" else 1) * int(instr[1:])) % 100
+    return password
+
+
+part_two(rotate_p2(rotations))
