dd62792180b8b6a27fcea5f5da980d567231db19
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-12-04 20:05:52 +0100

    Day 02 - Repeated sequences

--- AdventOfCode/2025/day02.py
+++ AdventOfCode/2025/day02.py
@@ -2,14 +2,36 @@ from libs import *
 
 # Parse input
 
-example = read("example")
+product_ranges = [[int(n) for n in r.split("-")] for r in read("example").split(",")]
 
 # Part 1
 
 
-part_one(example)
+def invalid_ids(start, end):
+    ids = []
+    for i in range(start, end + 1):
+        l = str(i)
+        if len(l) % 2 == 0:
+            if l[: len(l) // 2] == l[len(l) // 2 :]:
+                ids.append(int(l))
+    return ids
+
+
+part_one(sum([sum(invalid_ids(s, e)) for s, e in product_ranges]))
 
 # Part 2
 
 
-part_two(0)
+def invalid_ids_p2(start, end):
+    ids = []
+    for i in range(start, end + 1):
+        l = str(i)
+        for s in range(1, len(l) // 2 + 1):
+            if len(l) % s == 0:
+                if l == l[:s] * (len(l) // s):
+                    ids.append(int(l))
+                    break
+    return ids
+
+
+part_two(sum([sum(invalid_ids_p2(s, e)) for s, e in product_ranges]))
