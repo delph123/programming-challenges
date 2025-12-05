46b6a74b3ac42b3b4b48b4c45b02ce59be1f000c
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-12-05 22:13:13 +0100

    Day 03 - Biggest ordered sequence

--- AdventOfCode/2025/day03.py
+++ AdventOfCode/2025/day03.py
@@ -2,14 +2,23 @@ from libs import *
 
 # Parse input
 
-example = read("example")
+banks = [[int(n) for n in r] for r in read_lines("example")]
 
 # Part 1
 
 
-part_one(example)
+def largest_joltage(row, digits):
+    if digits == 1:
+        return max(row)
+    else:
+        d = digits - 1
+        m = max(row[:-d])
+        return m * (10**d) + largest_joltage(row[row.index(m) + 1 :], d)
+
+
+part_one(sum([largest_joltage(b, 2) for b in banks]))
 
 # Part 2
 
 
-part_two(0)
+part_two(sum([largest_joltage(b, 12) for b in banks]))
