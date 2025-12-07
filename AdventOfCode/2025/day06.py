e5940cb292b0f207a36b101ec2fecb349bf54f6f
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-12-07 22:42:30 +0100

    Day 06 - Transposition

--- AdventOfCode/2025/day06.py
+++ AdventOfCode/2025/day06.py
@@ -2,14 +2,44 @@ from libs import *
 
 # Parse input
 
-example = read("example")
+worksheet = read_lines("example")
+
+# Fix because the last spaces could get lost!
+line_size = max(len(l) for l in worksheet)
+worksheet = [row + " " * (line_size - len(row)) for row in worksheet]
 
 # Part 1
 
 
-part_one(example)
+def transpose_p1(worksheet):
+    problems = transpose([l.split() for l in worksheet])
+    return [(p[-1], [int(n) for n in p[:-1]]) for p in problems]
+
+
+def compute(op, numbers):
+    if op == "+":
+        return sum(numbers)
+    else:
+        return prod(numbers)
+
+
+part_one(sum(compute(op, n) for op, n in transpose_p1(worksheet)))
 
 # Part 2
 
 
-part_two(0)
+def transpose_p2(worksheet):
+    problems = [[None, []]]
+    for n, op in [
+        (row[:-1].strip(), row[-1]) for row in reversed(transpose(worksheet))
+    ]:
+        if n:
+            if op != " ":
+                problems[-1][0] = op
+            problems[-1][1].append(int(n))
+        else:
+            problems.append([None, []])
+    return problems
+
+
+part_two(sum(compute(op, n) for op, n in transpose_p2(worksheet)))
