f6b7cd06036d2a060b2ba3adfa4ae42b4c82d1e6
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-12-07 20:59:26 +0100

    Day 04 - Counting neighbors

--- AdventOfCode/2025/day04.py
+++ AdventOfCode/2025/day04.py
@@ -2,14 +2,33 @@ from libs import *
 
 # Parse input
 
-example = read("example")
+rolls_grid = read_grid("example")
 
 # Part 1
 
 
-part_one(example)
+def neighboring_rolls(g: Grid, p: Point):
+    return sum(1 for d in Point.DIRS.values() if g.get(p + d) == "@")
+
+
+def mark_accessible(g: Grid, mark):
+    g2 = g.copy()
+    for p in g:
+        if g[p] == "@" and neighboring_rolls(g, p) < 4:
+            g2[p] = mark
+    return g2
+
+
+part_one(mark_accessible(rolls_grid, "x").count("x"))
 
 # Part 2
 
 
-part_two(0)
+def mark_removable(g: Grid, mark):
+    g1, g2 = Grid([]), g
+    while g1 != g2:
+        g1, g2 = g2, mark_accessible(g2, mark)
+    return g2
+
+
+part_two(mark_removable(rolls_grid, "+").count("+"))
