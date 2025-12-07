a56c2edc27a435b12598847f1a965b6a8c7a8739
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-12-07 23:26:16 +0100

    Day 07 - Splitting light

--- AdventOfCode/2025/day07.py
+++ AdventOfCode/2025/day07.py
@@ -2,14 +2,43 @@ from libs import *
 
 # Parse input
 
-example = read("example")
+tachyon_manifolds = read_lines("example")
+source = tachyon_manifolds[0].find("S")
 
 # Part 1
 
 
-part_one(example)
+def split_beam(levels, beams):
+    splits = []
+    for n, level in enumerate(levels):
+        out_beams = set()
+        for beam in beams:
+            if level[beam] == "^":
+                splits.append((n, beam))
+                out_beams.add(beam - 1)
+                out_beams.add(beam + 1)
+            else:
+                out_beams.add(beam)
+        beams = out_beams
+    return splits
+
+
+part_one(len(split_beam(tachyon_manifolds, set([source]))))
 
 # Part 2
 
 
-part_two(0)
+def multi_split_beam(levels, beams: dict[int, int]):
+    for level in levels:
+        out_beams = defaultdict(int)
+        for beam, num in beams.items():
+            if level[beam] == "^":
+                out_beams[beam - 1] += num
+                out_beams[beam + 1] += num
+            else:
+                out_beams[beam] += num
+        beams = out_beams
+    return beams
+
+
+part_two(sum(multi_split_beam(tachyon_manifolds, {source: 1}).values()))
