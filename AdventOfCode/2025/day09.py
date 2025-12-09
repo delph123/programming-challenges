dd2a9d4d3b76f77688aa791163ce7c740729d361
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-12-09 17:51:42 +0100

    Day 09 - Inner rectangles

--- AdventOfCode/2025/day09.py
+++ AdventOfCode/2025/day09.py
@@ -2,12 +2,20 @@ from libs import *
 
 # Parse input
 
-example = read("example")
+red_tiles = [tuple(int(n) for n in l.split(",")) for l in read_lines("i")]
 
 # Part 1
 
 
-part_one(example)
+def largest_rectangle(corners):
+    return max(
+        (x - u + 1) * (y - v + 1)
+        for i, (x, y) in enumerate(corners)
+        for (u, v) in corners[i + 1 :]
+    )
+
+
+part_one(largest_rectangle(red_tiles))
 
 # Part 2
 
