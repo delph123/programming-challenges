07978da909c4501d094ca093f97c8fde57344e8f
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-12-25 18:01:19 +0100

    Day 02 - Counting letters

--- AdventOfCode/2018/day02.py
+++ AdventOfCode/2018/day02.py
@@ -2,12 +2,38 @@ from libs import *
 
 # Parse input
 
-data = read("e")
+boxes = read("example")
+
+if read.from_example:
+    boxes, boxes_p2 = boxes.split("\n\n")
+    boxes = boxes.splitlines()[1:]
+    boxes_p2 = boxes_p2.splitlines()[1:]
+else:
+    boxes = boxes.splitlines()
+    boxes_p2 = boxes
 
 # Part 1
 
-part_one(data)
+
+def replicate(box, times):
+    return times in Counter(box).values()
+
+
+part_one(sum(replicate(b, 2) for b in boxes) * sum(replicate(b, 3) for b in boxes))
 
 # Part 2
 
-part_two(0)
+
+def common(b0, b1):
+    return "".join(x for x, y in zip(b0, b1) if x == y)
+
+
+def most_similar(labels):
+    return max(
+        (common(b0, b1) for i, b0 in enumerate(labels) for b1 in labels[i + 1 :]),
+        key=len,
+    )
+
+
+# The boxes with most similar labels are the ones which differs from a single character
+part_two(most_similar(boxes_p2))
