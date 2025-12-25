fbf68324cb3382534ef2b3b9d05496fc8949a22f
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-12-25 18:00:19 +0100

    Day 01 - Additions

--- AdventOfCode/2018/day01.py
+++ AdventOfCode/2018/day01.py
@@ -2,12 +2,24 @@ from libs import *
 
 # Parse input
 
-data = read("e")
+changes = [int(n) for n in read_lines("example")]
 
 # Part 1
 
-part_one(data)
+part_one(sum(changes))
 
 # Part 2
 
-part_two(0)
+
+def repeated_frequency(start):
+    frequencies = {start}
+    f = start
+    while True:
+        for c in changes:
+            f += c
+            if f in frequencies:
+                return f
+            frequencies.add(f)
+
+
+part_two(repeated_frequency(0))
