8a9c63cd40307b382f6a65e179c10738064d2324
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-12-26 16:48:36 +0100

    Day 08 - Multi-value tree

--- AdventOfCode/2018/day08.py
+++ AdventOfCode/2018/day08.py
@@ -2,12 +2,46 @@ from libs import *
 
 # Parse input
 
-data = read("e")
+license_numbers = [int(n) for n in read("example").split()]
 
 # Part 1
 
-part_one(data)
+
+def tree():
+    def subtree(index):
+        children = license_numbers[index]
+        mentries = license_numbers[index + 1]
+        nodes = []
+        index += 2
+        for _ in range(children):
+            node, index = subtree(index)
+            nodes.append(node)
+        metadata = license_numbers[index : index + mentries]
+        return ((nodes, metadata), index + mentries)
+
+    return subtree(0)[0]
+
+
+def traverse(t):
+    children, metadata = t
+    yield from metadata
+    for child in children:
+        yield from traverse(child)
+
+
+part_one(sum(traverse(tree())))
 
 # Part 2
 
-part_two(0)
+
+def value(t):
+    children, metadata = t
+    if len(children) == 0:
+        return sum(metadata)
+    indexes = Counter(metadata)
+    return sum(
+        value(children[c - 1]) * n for c, n in indexes.items() if 0 < c <= len(children)
+    )
+
+
+part_two(value(tree()))
