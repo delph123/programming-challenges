dc2de04ae79a020e02c0e0d79087a27def9902bd
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-12-26 17:27:07 +0100

    Day 09 - Zipper again (for circle)

--- AdventOfCode/2018/day09.py
+++ AdventOfCode/2018/day09.py
@@ -2,12 +2,39 @@ from libs import *
 
 # Parse input
 
-data = read("e")
+players, marbles = [
+    int(n)
+    for n in read("e", ignore=["players; last marble is worth", "points"]).split()
+]
 
 # Part 1
 
-part_one(data)
+
+def scores(players, marbles):
+    if marbles < 23:
+        return 0
+    lhs, rhs = [0, 2], [1]
+    scores = [0] * players
+    for m in range(3, marbles + 1):
+        if m % 23 != 0:
+            if len(rhs) == 0:
+                lhs, rhs = [], list(reversed(lhs))
+            lhs.append(rhs.pop())
+            lhs.append(m)
+        else:
+            for _ in range(8):
+                if len(lhs) == 0:
+                    lhs, rhs = list(reversed(rhs)), []
+                rhs.append(lhs.pop())
+            scores[(m - 1) % players] += m + rhs.pop()
+            if len(rhs) == 0:
+                lhs, rhs = [], list(reversed(lhs))
+            lhs.append(rhs.pop())
+    return scores
+
+
+part_one(max(scores(players, marbles)))
 
 # Part 2
 
-part_two(0)
+part_two(max(scores(players, 100 * marbles)))
