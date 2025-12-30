b9c6cefe9a37cb846cb3a6bb3c8ba7e48079dddf
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2025-12-30 01:03:24 +0100

    Day 14 - Find sequence of digits

--- AdventOfCode/2018/day14.py
+++ AdventOfCode/2018/day14.py
@@ -2,12 +2,40 @@ from libs import *
 
 # Parse input
 
-data = read("e")
+score = read("example").splitlines()
+
+nb_recipes = int(score[0])
+score = [int(n) for n in list(score[-1])]
 
 # Part 1
 
-part_one(data)
+
+def scores(nb_recipes):
+    recipes = [3, 7]
+    e1, e2 = (0, 1)
+    while len(recipes) < nb_recipes + 10:
+        recipes.extend(int(n) for n in str(recipes[e1] + recipes[e2]))
+        e1 = (e1 + recipes[e1] + 1) % len(recipes)
+        e2 = (e2 + recipes[e2] + 1) % len(recipes)
+    return recipes[nb_recipes : nb_recipes + 10]
+
+
+part_one(scores(nb_recipes), sep="")
 
 # Part 2
 
-part_two(0)
+
+def find_score(score):
+    recipes = [3, 7]
+    e1, e2 = (0, 1)
+    while recipes[-len(score) :] != score and recipes[-len(score) - 1 : -1] != score:
+        recipes.extend(int(n) for n in str(recipes[e1] + recipes[e2]))
+        e1 = (e1 + recipes[e1] + 1) % len(recipes)
+        e2 = (e2 + recipes[e2] + 1) % len(recipes)
+    if recipes[-len(score) - 1 : -1] == score:
+        return len(recipes) - len(score) - 1
+    else:
+        return len(recipes) - len(score)
+
+
+part_two(find_score(score))
